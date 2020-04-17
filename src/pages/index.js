b686ca3a80d2f7b1c2fbd5c89d9a293224eef5b7
nikitashrestha <nikshres522@gmail.com>
2020-04-17 19:23:47 +0545

    form-preview:#2 - added dummydata and PersonalInfo component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Header from '../components/header/Header';
 import UserDetail from '../components/userdetail/UserDetail';
+import PersonalInfo from '../components/preview/PersonalInfo';
 
 const App = () => {
+  // App state
+  const [preview, setPreview] = useState(false);
+
+  // Data source
+  const personalInfo = {
+    name: { data: 'Ribby McFroggy', visibility: true },
+    summary: { data: 'Hlelo', visibility: true },
+  };
+
+  const handleOnPreviewBtnClicked = e => {
+    e.preventDefault();
+    setPreview(!preview);
+  };
+
   return (
     <div className="page-container">
-      <Header />
-      <UserDetail />
+      <Header name={personalInfo.name.data} status="Employee" />
+      <UserDetail
+        name={personalInfo.name.data}
+        experience="5 years professional experience"
+        onPreviewBtnClicked={handleOnPreviewBtnClicked}
+      />
+      <PersonalInfo personalInfo={personalInfo} preview={preview}></PersonalInfo>
     </div>
   );
 };
