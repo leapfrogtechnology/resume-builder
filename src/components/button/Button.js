c667067eeb80e777d2963d7ebc592426d0fcfb53
nikitashrestha <nikshres522@gmail.com>
2020-04-17 19:04:24 +0545

    form-preview:#2 - added onclick prop and declared proptypes

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button = ({ content }) => {
-  return <button className="btn">{content}</button>;
+const Button = props => {
+  const { content, onclick } = props;
+
+  return (
+    <button className="btn" onClick={e => onclick(e)}>
+      {content}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  content: PropTypes.string,
+  onclick: PropTypes.func,
 };
 
 export default Button;
