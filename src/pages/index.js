e40dfa8ef440405d1aa16310a157ace3809383d9
Esha-Rajbhandari <esha9277@gmail.com>
2020-04-17 11:50:13 +0545

    RB-design: #13- Sass setup for the resume builder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Header from '../components/header/Header';
 
 const App = () => {
-  return <h1>Resume Builder</h1>;
+  return(
+    <Header />
+  );
 };
 
 export default App;
