12306556ec552674e3090421e498492996e8e19b
nikitashrestha <nikshres522@gmail.com>
2020-04-20 10:02:42 +0545

    form-preview:#2 - added new line

--- src/components/icons/icon.js
+++ src/components/icons/icon.js
@@ -11,6 +11,7 @@ export const FAVORITE_ICON = (
     />
   </svg>
 );
+
 export const DROPDOWN = (
   <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
