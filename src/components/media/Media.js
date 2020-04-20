4da7b49af73b7341780de3be38583e4812fa85bf
nikitashrestha <nikshres522@gmail.com>
2020-04-20 09:28:24 +0545

    form-preview:#2 - added destructure in the props

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Media = props => {
-  const { icon, label } = props;
-
+const Media = ({ icon, label }) => {
   return (
     <div className="media">
       <span className="media__icon">{icon}</span>
--- src/components/preview/PersonalInfo.js
+++ src/components/preview/PersonalInfo.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { useState } from 'react';
-
-const PersonalInfo = props => {
-  const { personalInfo, preview } = props;
-
+const PersonalInfo = ({ personalInfo, preview }) => {
   const personalInfoList = Object.keys(personalInfo).map(key => (
     <PersonalInfoItem key={key} title={key} data={personalInfo[key]} preview={preview}></PersonalInfoItem>
   ));
@@ -24,60 +20,6 @@ const PersonalInfo = props => {
 PersonalInfo.propTypes = {
   personalInfo: PropTypes.object,
   preview: PropTypes.bool,
-};
-
-const PersonalInfoItem = props => {
-  const { title, data, preview } = props;
-
-  const [hidden, setHidden] = useState(0);
-
-  return data ? (
-    <li>
-      <div>
-        <div>
-          <span>Your {title.charAt(0).toUpperCase() + title.slice(1)}</span>
-          <p>{data.data}</p>
-          {hidden ? <span>Hidden</span> : <></>}
-        </div>
-        {preview ? (
-          <></>
-        ) : (
-          <div>
-            <button
-              onClick={e => {
-                setHidden(!hidden);
-                onHiddenClicked(e, hidden);
-              }}
-            >
-              {hidden ? 'Show' : 'Hide'}
-            </button>
-          </div>
-        )}
-      </div>
-    </li>
-  ) : (
-    <></>
-  );
-};
-
-PersonalInfoItem.propTypes = {
-  title: PropTypes.string,
-  data: PropTypes.object,
-  preview: PropTypes.bool,
-};
-
-/**
- * @param {Event} _e
- *  @param {boolean} hidden
- */
-const onHiddenClicked = (_e, hidden) => {
-  // Here we call our api to manipulate our json
-
-  if (!hidden) {
-    // console.log('Removed');
-  } else {
-    // console.log('added');
-  }
 };
 
 export default PersonalInfo;
