d8c36255c4254194e99d85a04aa4970920ded2cb
nikitashrestha <nikshres522@gmail.com>
2020-04-20 20:22:57 +0545

    form-review:#2 - add a line

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withImages = require('next-images');
 const path = require('path');
 const webpack = require('webpack');
+
 module.exports = withImages({
   webpack: config => {
     config.resolve.alias['~'] = path.resolve('src');
