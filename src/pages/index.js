8f303df99a01d3b19959971f9aebeb4db2d22dec
Esha-Rajbhandari <esha9277@gmail.com>
2020-04-20 14:06:59 +0545

    RB-design: Add title to the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from '~/components/header/Header';
 import Dashboard from '~/components/dashboard/Dashboard';
+import Head from 'next/head';
 
 const App = () => {
   return (
     <div className="page-container">
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>ResumeBuilder</title>
+      </Head>
       <Header />
       <Dashboard />
     </div>
