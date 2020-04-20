0a229bed301f6df9b64f1b997b934c61e375bd84
nikitashrestha <nikshres522@gmail.com>
2020-04-20 18:15:23 +0545

    form-preview:#2 - updated imports and proptypes

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Skills from '~/components/skills/Skills';
 import Sidenav from '~/components/sidenav/Sidenav';
 import Certificate from '~/components/certificate/Certificate';
@@ -7,22 +9,26 @@ import WorkExperience from '~/components/workexperience/WorkExperience';
 import ProjectsUndertaken from '~/components/projectsundertaken/ProjectsUndertaken';
 import PersonalInformation from '~/components/personalinformation/PersonalInformation';
 
-const Dashboard = () => {
+const Dashboard = ({ profile, preview }) => {
   return (
     <section className="container">
       <div className="main-container">
         <div className="main-content">
-          <PersonalInformation />
+          <PersonalInformation personalInformation={profile.personalInformation} preview={preview} />
           <Skills />
           <WorkExperience />
           <ProjectsUndertaken />
-          <Achievements />
+          <Achievements achievements={profile.achievements} preview={preview} />
           <Certificate />
         </div>
         <Sidenav />
       </div>
     </section>
   );
+};
+
+Dashboard.propTypes = {
+  profile: PropTypes.object,
 };
 
 export default Dashboard;
