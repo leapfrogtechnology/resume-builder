c62ffb653a21ca2f6abac1469230714fcc8694e4
Esha-Rajbhandari <esha9277@gmail.com>
2020-04-21 14:51:18 +0545

    RB-form: #1 - removed unnecessary imports from dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import Skills from '~/components/skills/Skills';
 import Sidenav from '~/components/sidenav/Sidenav';
-import AddSkill from '~/components/form/skill/AddSkill';
-import AddProject from '~/components/form/project/AddProject';
 import Certificate from '~/components/certificate/Certificate';
 import Achievements from '~/components/achievements/Achievements';
 import WorkExperience from '~/components/workexperience/WorkExperience';
-import AddContactInformation from '~/components/form/contact/AddContact';
-import AddAchievement from '~/components/form/achievement/AddAchievement';
-import AddCertificate from '~/components/form/certificate/AddCertificate';
-import AddWorkExperience from '~/components/form/workexperience/AddWordExperience';
 import ProjectsUndertaken from '~/components/projectsundertaken/ProjectsUndertaken';
 import PersonalInformation from '~/components/personalinformation/PersonalInformation';
-import AddPersonalInformation from '~/components/form/personalinfornation/AddPersonalInformation';
 
 const Dashboard = () => {
 	return (
@@ -25,13 +18,6 @@ const Dashboard = () => {
 					<ProjectsUndertaken />
 					<Achievements />
 					<Certificate />
-					{/* <AddWorkExperience /> */}
-					{/* <AddAchievement /> */}
-					{/* <AddCertificate /> */}
-					{/* <AddPersonalInformation /> */}
-					{/* <AddContactInformation /> */}
-					{/* <AddSkill /> */}
-					<AddProject />
 				</div>
 				<Sidenav />
 			</div>
