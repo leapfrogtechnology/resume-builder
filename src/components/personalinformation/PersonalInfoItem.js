e1c86f16155567624c652bbfc3aaab5c1bfb6c1e
nikitashrestha <nikshres522@gmail.com>
2020-04-21 14:50:43 +0545

    form-preview:#2 - remove uppercase implementation

--- src/components/personalinformation/PersonalInfoItem.js
+++ src/components/personalinformation/PersonalInfoItem.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { View, ViewHidden } from '~/assets/image';
 
-const PersonalInfoItem = ({ label, value, visibility, bold, preview }) => {
+const PersonalInfoItem = ({ label, value, hidden = false, bold, preview }) => {
+  if (preview && hidden) {
+    return null;
+  }
   return (
     <div className="personal-info-row">
       <div className="personal-info-row__item">
-        <div className="personal-info-row__label">Your {label.charAt(0).toUpperCase() + label.slice(1)}</div>
+        <div className="personal-info-row__label">Your {label}</div>
         <p className={bold ? 'personal-info-row__value  personal-info-row__value--strong' : 'personal-info-row__value'}>
           {value}
-          {!visibility && <span className="hidden-tag">Hidden</span>}
+          {hidden && <span className="hidden-tag">Hidden</span>}
         </p>
       </div>
       {!preview && (
         <div className="personal-info-row__icon">
-          <img src={visibility ? View : ViewHidden} alt="View" />
+          <img src={!hidden ? View : ViewHidden} alt="View" />
         </div>
       )}
     </div>
--- src/components/personalinformation/PersonalInformation.js
+++ src/components/personalinformation/PersonalInformation.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { Edit } from '~/assets/image';
 import PersonalInfoItem from './PersonalInfoItem';
 import CardHeader from '~/components/cardheader/CardHeader';
 
-const PersonalInformation = ({ personalInformation, preview }) => {
-  const personalInfoList = Object.keys(personalInformation).map(key => (
-    <PersonalInfoItem
-      key={key}
-      label={key}
-      value={personalInformation[key].data}
-      preview={preview}
-      bold={personalInformation[key].bold}
-      visibility={personalInformation[key].visibility}
-    ></PersonalInfoItem>
-  ));
+const PersonalInformation = ({ name, role, introduction, preview }) => {
+  const bold = true;
 
   return (
     <div className="personal-info-block">
       <div className="card">
         <CardHeader title="Personal Information" icon={!preview ? Edit : ''} />
-        {personalInfoList}
+        {name && <PersonalInfoItem label="Name" value={name} preview={preview} bold={bold}></PersonalInfoItem>}
+        {role && (
+          <PersonalInfoItem
+            label="Role"
+            value={role.label}
+            preview={preview}
+            bold={bold}
+            hidden={role.hidden}
+          ></PersonalInfoItem>
+        )}
+        {introduction && (
+          <PersonalInfoItem
+            label="Introduction"
+            value={introduction.value}
+            preview={preview}
+            bold={!bold}
+            hidden={introduction.hidden}
+          ></PersonalInfoItem>
+        )}
       </div>
     </div>
   );
 };
 
 PersonalInformation.propTypes = {
-  personalInformation: PropTypes.object,
+  name: PropTypes.object,
+  role: PropTypes.object,
+  introduction: PropTypes.object,
   preview: PropTypes.bool,
 };
 
