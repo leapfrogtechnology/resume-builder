2ecb9d98450f0c4f6e4027319b5020c3d8b14b6d
Esha-Rajbhandari <esha9277@gmail.com>
2020-04-21 12:42:13 +0545

    RB-form: #1 - add input partial

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -31,7 +31,7 @@ const Dashboard = () => {
 					{/* <AddPersonalInformation /> */}
 					{/* <AddContactInformation /> */}
 					{/* <AddSkill /> */}
-					{/* <AddProject /> */}
+					<AddProject />
 				</div>
 				<Sidenav />
 			</div>
