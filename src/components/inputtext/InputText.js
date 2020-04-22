671b4f55dabd5d241ddfeee0e4742b15fa300f6c
Yankee <yankee.exe@gmail.com>
2020-04-22 12:15:04 +0545

    Dynamic input field  render based on  props

--- src/components/inputtext/InputText.js
+++ src/components/inputtext/InputText.js
@@ -1,12 +1,31 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { useField } from 'formik';
 
-const InputText = ({label, placeholder}) => {
-  return(
+const InputText = ({ label, placeholder, ...props }) => {
+  const [field, meta] = useField(props);
+
+  const textArea = (
+    <textarea
+      className="input__textarea"
+      cols="30"
+      rows="10"
+      placeholder={placeholder}
+      {...field}
+      {...props}
+    ></textarea>
+  );
+
+  const inputField = <input className="input__text-field" placeholder={placeholder} {...field} {...props} />;
+
+  return (
     <div className="input">
       <label className="input__label">{label}</label>
-      <input type="text" className="input__text-field" placeholder={placeholder}/>
+      {props.type && props.type === 'text-area' ? textArea : inputField}
+      {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
     </div>
-  )
-}
+  );
+};
 
 export default InputText;
