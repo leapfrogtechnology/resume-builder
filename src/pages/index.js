453260291c7f9bb425f349a2df71a71cc586f4e3
Yankee <yankee.exe@gmail.com>
2020-04-22 15:08:05 +0545

    Camelcase attribute name

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const App = () => {
       <Head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>ResumeBuilder</title>
       </Head>
       <Header />
