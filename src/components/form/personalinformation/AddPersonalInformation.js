c8d7950cc9420246350a8afecf06193947d424f0
Yankee <yankee.exe@gmail.com>
2020-04-22 12:13:03 +0545

    Add formik components and Yup validation

--- src/components/form/personalinformation/AddPersonalInformation.js
+++ src/components/form/personalinformation/AddPersonalInformation.js
@@ -1,14 +1,23 @@
+/* eslint-disable require-jsdoc */
+import * as Yup from 'yup';
 import React, { Component } from 'react';
+import { Formik, Form } from 'formik';
+
 import { Close, Trash } from '~/assets/image';
 import Button from '~/components/button/Button';
 import InputText from '~/components/inputtext/InputText';
 import FormHeader from '~/components/formheader/FormHeader';
-import InputTextArea from '~/components/inputtextarea/InputTextArea';
 
 class AddPersonalInformation extends Component {
   constructor({ onClose }) {
     super();
   }
+
+  validatePersonalInformation = Yup.object().shape({
+    name: Yup.string().min(15, 'Above 15 characters.').max(35, 'Limit 35 characters').required(),
+    role: Yup.string().max(40, 'Limit 40 characters').required(),
+    introduction: Yup.string().max(200, 'Limit 200 characters').required(),
+  });
 
   render() {
     return (
@@ -19,19 +28,30 @@ class AddPersonalInformation extends Component {
               <img src={Close} alt="close" />
             </div>
             <FormHeader title="Personal Information" />
-            <div className="form__content">
-              <InputText label="Your Name" />
-              <InputText label="Your Role" />
-              <InputTextArea label="Your Information" />
-              <div className="form-button">
-                <div className="form-button__left">
-                  <Button content="Save Info" />
+            <Formik
+              initialValues={{
+                name: '',
+                role: '',
+                introduction: '',
+              }}
+              validationSchema={this.validatePersonalInformation}
+            >
+              <Form>
+                <div className="form__content">
+                  <InputText name="name" label="Your Name" />
+                  <InputText name="role" label="Your Role" />
+                  <InputText name="introduction" label="Your Introduction" type="text-area" />
+                  <div className="form-button">
+                    <div className="form-button__left">
+                      <Button content="Save Info" />
+                    </div>
+                    <div className="form-button__right">
+                      <Button content="Cancel" isCancel={true} />
+                    </div>
+                  </div>
                 </div>
-                <div className="form-button__right">
-                  <Button content="Cancel" isCancel={true} />
-                </div>
-              </div>
-            </div>
+              </Form>
+            </Formik>
           </div>
         </div>
       </div>
