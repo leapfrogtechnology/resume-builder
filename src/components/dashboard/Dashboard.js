dc49831d92c2e1991058c14cbd763f7e51677dab
Yankee <yankee.exe@gmail.com>
2020-04-22 14:59:57 +0545

    Create FormContext, Add provide at Dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Skills from '~/components/skills/Skills';
 import Sidenav from '~/components/sidenav/Sidenav';
 import Certificate from '~/components/certificate/Certificate';
@@ -7,22 +7,29 @@ import WorkExperience from '~/components/workexperience/WorkExperience';
 import ProjectsUndertaken from '~/components/projectsundertaken/ProjectsUndertaken';
 import PersonalInformation from '~/components/personalinformation/PersonalInformation';
 
+import { FormContext } from '../FormContext';
+
 const Dashboard = () => {
-	return (
-		<section className="container">
-			<div className="main-container">
-				<div className="main-content">
-					<PersonalInformation />
-					<Skills />
-					<WorkExperience />
-					<ProjectsUndertaken />
-					<Achievements />
-					<Certificate />
-				</div>
-				<Sidenav />
-			</div>
-		</section>
-	)
-}
+  const [data, setData] = useState({});
+
+  return (
+    <section className="container">
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <div className="main-container">
+        <div className="main-content">
+          <FormContext.Provider value={{ data, setData }}>
+            <PersonalInformation />
+            <Skills />
+            <WorkExperience />
+            <ProjectsUndertaken />
+            <Achievements />
+            <Certificate />
+          </FormContext.Provider>
+        </div>
+        <Sidenav />
+      </div>
+    </section>
+  );
+};
 
 export default Dashboard;
