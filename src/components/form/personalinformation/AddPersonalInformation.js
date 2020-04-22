1698c11312d6c559047ff8317177490a73dd9d49
Yankee <yankee.exe@gmail.com>
2020-04-22 15:04:31 +0545

    UseContext and Formik wrapper

--- src/components/form/personalinformation/AddPersonalInformation.js
+++ src/components/form/personalinformation/AddPersonalInformation.js
@@ -1,62 +1,62 @@
 /* eslint-disable require-jsdoc */
 import * as Yup from 'yup';
-import React, { Component } from 'react';
 import { Formik, Form } from 'formik';
+import React, { useContext } from 'react';
 
 import { Close, Trash } from '~/assets/image';
 import Button from '~/components/button/Button';
+import { FormContext } from '../../FormContext';
 import InputText from '~/components/inputtext/InputText';
 import FormHeader from '~/components/formheader/FormHeader';
 
-class AddPersonalInformation extends Component {
-  constructor({ onClose }) {
-    super();
-  }
+const AddPersonalInformation = ({ onClose }) => {
+  const { data, setData } = useContext(FormContext);
 
-  validatePersonalInformation = Yup.object().shape({
+  const validatePersonalInformation = Yup.object().shape({
     name: Yup.string().min(15, 'Above 15 characters.').max(35, 'Limit 35 characters').required(),
     role: Yup.string().max(40, 'Limit 40 characters').required(),
     introduction: Yup.string().max(200, 'Limit 200 characters').required(),
   });
 
-  render() {
-    return (
-      <div className="form-container">
-        <div className="form">
-          <div className="card">
-            <div className="close-option" onClick={this.props.onClose}>
-              <img src={Close} alt="close" />
-            </div>
-            <FormHeader title="Personal Information" />
-            <Formik
-              initialValues={{
-                name: '',
-                role: '',
-                introduction: '',
-              }}
-              validationSchema={this.validatePersonalInformation}
-            >
-              <Form>
-                <div className="form__content">
-                  <InputText name="name" label="Your Name" />
-                  <InputText name="role" label="Your Role" />
-                  <InputText name="introduction" label="Your Introduction" type="text-area" />
-                  <div className="form-button">
-                    <div className="form-button__left">
-                      <Button content="Save Info" />
-                    </div>
-                    <div className="form-button__right">
-                      <Button content="Cancel" isCancel={true} />
-                    </div>
+  return (
+    <div className="form-container">
+      <div className="form">
+        <div className="card">
+          <div className="close-option" onClick={onClose}>
+            <img src={Close} alt="close" />
+          </div>
+          <FormHeader title="Personal Information" />
+          <Formik
+            initialValues={{
+              name: '',
+              role: '',
+              introduction: '',
+            }}
+            onSubmit={values => {
+              setData(prevState => ({ ...prevState, ...values }));
+            }}
+            validationSchema={validatePersonalInformation}
+          >
+            <Form>
+              <div className="form__content">
+                <InputText name="name" label="Your Name" />
+                <InputText name="role" label="Your Role" />
+                <InputText name="introduction" label="Your Introduction" type="text-area" />
+                <div className="form-button">
+                  <div className="form-button__left">
+                    <Button content="Save Info" />
+                  </div>
+                  <div className="form-button__right">
+                    <Button content="Cancel" isCancel={true} />
                   </div>
                 </div>
-              </Form>
-            </Formik>
-          </div>
+              </div>
+            </Form>
+          </Formik>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default AddPersonalInformation;
--- src/components/form/skill/AddSkill.js
+++ src/components/form/skill/AddSkill.js
@@ -1,41 +1,54 @@
-import React, {Component} from 'react';
+/* eslint-disable require-jsdoc */
+/* eslint-disable react/prop-types */
+import React, { useContext } from 'react';
+import { Formik, Form } from 'formik';
+
 import { Close, Trash } from '~/assets/image';
 import Button from '~/components/button/Button';
 import InputText from '~/components/inputtext/InputText';
 import FormHeader from '~/components/formheader/FormHeader';
 import InputSelect from '~/components/inputselect/InputSelect';
+import { FormContext } from '../../FormContext';
 
-class AddSkill extends Component {
-  constructor({ onClose }) {
-    super();
-  }
+const AddSkill = ({ onClose }) => {
+  const { data, setData } = useContext(FormContext);
 
-  render(){
-    return (
-      <div className="form-container">
-        <div className="form">
-          <div className="card">
-            <div className="close-option" onClick={this.props.onClose}>
-              <img src={Close} alt="close" />
-            </div>
-            <FormHeader title="Add Skill" />
-            <div className="form__content">
-              <InputSelect label="Select your skill"/>
-              <InputText label="Add Sub Skill" />
-              <div className="form-button">
-                <div className="form-button__left">
-                  <Button content="Add Skill" />
-                </div>
-                <div className="form-button__right">
-                  <Button content="Cancel" isCancel={true} />
+  return (
+    <div className="form-container">
+      <div className="form">
+        <div className="card">
+          <div className="close-option" onClick={onClose}>
+            <img src={Close} alt="close" />
+          </div>
+          <FormHeader title="Add Skill" />
+          <Formik
+            initialValues={{
+              skill: '',
+              subSkills: [],
+            }}
+            onSubmit={values => {
+              setData(prevState => ({ ...prevState, ...values }));
+            }}
+          >
+            <Form>
+              <div className="form__content">
+                <InputSelect name="skill" label="Select your skill" />
+                <InputText name="subSkills" label="Add Sub Skill" />
+                <div className="form-button">
+                  <div className="form-button__left">
+                    <Button content="Add Skill" />
+                  </div>
+                  <div className="form-button__right">
+                    <Button content="Cancel" isCancel={true} />
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
+            </Form>
+          </Formik>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
 export default AddSkill;
