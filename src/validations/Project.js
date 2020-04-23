7da3f047517b7b8c0d1339f0d234a537deb52747
nikitashrestha <nikshres522@gmail.com>
2020-04-23 18:21:49 +0545

    modal-preview:#1 - add validation for addProject modal

---
+++ src/validations/Project.js
@@ -0,0 +1,15 @@
+import * as Yup from 'yup';
+
+const validateProjectInformation = Yup.object().shape({
+  name: Yup.string().label('Title of your Project').trim().min(10).max(30).required(),
+  ongoing: Yup.boolean().label('Current work'),
+  startDate: Yup.date().label('Start date').required(),
+  endDate: Yup.date()
+    .label('End Date')
+    .when('ongoing', { is: true, otherwise: Yup.date().required('This is required') })
+    .when('startDate', (startDate, schema) => startDate && schema.min(startDate)),
+  type: Yup.string().required(),
+  description: Yup.string().label('Roles and Responsibilities').min(60).max(200).required(),
+});
+
+export default validateProjectInformation;
