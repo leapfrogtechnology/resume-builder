1ec6248e995940176a3b4eabf78c64bbc52cb725
nikitashrestha <nikshres522@gmail.com>
2020-04-23 20:08:06 +0545

    form-preview:#1 - remove unnecessary curly bracket

--- src/components/inputdate/InputDate.js
+++ src/components/inputdate/InputDate.js
@@ -5,7 +5,7 @@ const InputDate = ({ label, placeholder, modifier }) => {
   return (
     <div className={modifier ? 'input--short' : 'input'}>
       <label className="input__label">{label}</label>
-      <div className={'input__date'}>
+      <div className="input__date">
         <input type="date" className="input__date-field" placeholder={placeholder} />
         <img src={Calendar} alt="Calendar" />
       </div>
