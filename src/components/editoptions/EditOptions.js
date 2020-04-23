b24909c0501237df1664b0a81fdf9e3f90c9dde5
nikitashrestha <nikshres522@gmail.com>
2020-04-23 18:23:49 +0545

    modal-preview:#1 - add onEdit handler

--- src/components/editoptions/EditOptions.js
+++ src/components/editoptions/EditOptions.js
@@ -3,34 +3,32 @@ import OpenModal from '~/components/modal/OpenModal';
 import { View, EditGray, Trash, ViewHidden } from '~/assets/image';
 
 class EditOptions extends Component {
-	constructor({ isHidden, component, onEdit, onClose, showModal }) {
-		super();
+  constructor({ isHidden, component, onEdit, onClose, showModal }) {
+    super();
 
-		this.state = {
-			isHidden: isHidden,
-			showModal: false,
-			component: component
-		}
-	}
+    this.state = {
+      isHidden: isHidden,
+      showModal: false,
+      component: component,
+    };
+  }
 
-	render() {
-		return (
-			<div className="edit-options">
-				<span className="edit-options__item">
-					<img src={this.state.isHidden ? ViewHidden : View} alt="View" />
-				</span>
-				<span className="edit-options__item" onClick={e => this.props.onEdit(e)}>
-					<img src={EditGray} alt="Edit" />
-				</span>
-				<span className="edit-options__item">
-					<img src={Trash} alt="Trash" />
-				</span>
-				{
-					this.props.showModal ? <OpenModal component={this.state.component} onClose={this.props.onClose}/> : ''
-				}
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="edit-options">
+        <span className="edit-options__item">
+          <img src={this.state.isHidden ? ViewHidden : View} alt="View" />
+        </span>
+        <span className="edit-options__item" onClick={e => this.props.onEdit(e)}>
+          <img src={EditGray} alt="Edit" />
+        </span>
+        <span className="edit-options__item">
+          <img src={Trash} alt="Trash" />
+        </span>
+        {this.props.showModal ? <OpenModal component={this.state.component} onClose={this.props.onClose} /> : ''}
+      </div>
+    );
+  }
 }
 
 export default EditOptions;
