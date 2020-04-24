54e46e85e8a5d884362c71c4ff0eb62b4010b5ff
nikitashrestha <nikshres522@gmail.com>
2020-04-24 15:39:21 +0545

    modal-preview:#1 - add validation schema for contact

---
+++ src/validations/Contact.js
@@ -0,0 +1,35 @@
+import * as Yup from 'yup';
+import { emailCheck, phoneNumberCheck } from '~/common/constants';
+
+const validateContactInformation = Yup.object().shape({
+  email: Yup.string()
+    .label('Email')
+    .required('Email is required')
+    .test('test-name', 'Enter a valid Email', function (value) {
+      const emailRegex = emailCheck;
+      const isValidEmail = emailRegex.test(value);
+
+      if (!isValidEmail) {
+        return false;
+      }
+
+      return true;
+    }),
+  contact: Yup.date()
+    .label('Phone Number')
+    .test('test-name', 'Enter a valid Phone Number', function (value) {
+      const phoneRegex = phoneNumberCheck;
+      const isValidPhone = phoneRegex.test(value);
+
+      if (!isValidPhone) {
+        return false;
+      }
+
+      return true;
+    }),
+  gitHub: Yup.string().label('Github Profile'),
+  stackOverFlow: Yup.string().label('StackOverflow profile'),
+  linkedIn: Yup.string().label('LinkedIn profile'),
+});
+
+export default validateContactInformation;
