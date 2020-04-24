3066d0be976d0c8073db4563ca73ef7d3b8d5c81
nikitashrestha <nikshres522@gmail.com>
2020-04-24 15:42:48 +0545

    modal-preview:#1 - update validation schema for certificate

--- src/validations/Certificate.js
+++ src/validations/Certificate.js
@@ -1,8 +1,8 @@
 import * as Yup from 'yup';
 
 const validateCertificateInformation = Yup.object().shape({
-  name: Yup.string().label('Title of your Certificate').trim().min(10).max(30).required(),
-  link: Yup.string().label('Link to this Certificate').trim().min(10).max(30).required(),
+  name: Yup.string().label('Title of your Certificate').trim().min(10).max(50).required(),
+  link: Yup.string().label('Link to this Certificate').trim().min(10).max(200).required(),
   date: Yup.date().label('Date you received the Certificate').required(),
   description: Yup.string().label('Describe this Certificate (optional)'),
 });
