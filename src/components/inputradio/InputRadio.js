e93c72936b0524a5ca0572f5fce147988b7bfef5
nikitashrestha <nikshres522@gmail.com>
2020-04-24 11:58:15 +0545

    modal-preview:#1 - add onchange method

--- src/components/inputradio/InputRadio.js
+++ src/components/inputradio/InputRadio.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { useField } from 'formik';
+import PropTypes from 'prop-types';
 
-const InputRadio = ({ label, value, placeholder }) => {
+const InputRadio = ({ label, value, placeholder, ...props }) => {
+  const [field, meta, form] = useField(props);
+
+  const change = (e, _name, _requireValidation) => {
+    const inputValue = e.target.value;
+
+    form.setValue(inputValue);
+  };
+
   return (
     <div className="input__label-container">
-      <input type="radio" className="input__radio-field" value={value} />
+      <input
+        type="radio"
+        className="input__radio-field"
+        value={value}
+        onChange={e => change(e, field.name, true)}
+        {...props}
+      />
       <label className="input__radio-label">{placeholder}</label>
+      {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
     </div>
-  )
-}
+  );
+};
+
+InputRadio.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+};
 
 export default InputRadio;
