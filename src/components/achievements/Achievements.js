2a97eb71f8de2dc99eee9072970e000f4935f313
nikitashrestha <nikshres522@gmail.com>
2020-04-24 12:22:56 +0545

    modal-preview:#1 - add formik and validation

--- src/components/achievements/Achievements.js
+++ src/components/achievements/Achievements.js
@@ -1,27 +1,46 @@
-import React from 'react';
-import CardHeader from '~/components/cardheader/CardHeader';
-import EditOptions from '~/components/editoptions/EditOptions';
+import React, { useState } from 'react';
+
 import { Add } from '~/assets/image';
+import CardHeader from '~/components/cardheader/CardHeader';
 import CardFooter from '~/components/cardfooter/CardFooter';
+import EditOptions from '~/components/editoptions/EditOptions';
+import AddAchievement from '~/components/form/achievement/AddAchievement';
 
 const Achievements = () => {
-	return (
-		<div className="achievements-block">
-			<div className="card">
-				<CardHeader title="Achievements" />
-				<div className="achievements">
-					<div className="achievements__row">
-						<div className="achievements__row-header">
-							<div className="sub-title">Headhunt Award</div>
-							<EditOptions />
-						</div>
-						<div className="achievements__year">December 2012</div>
-					</div>
-				</div>
-				<CardFooter icon={Add} label="Add another achievement" />
-			</div>
-		</div>
-	)
-}
+  const [showModel, setModel] = useState(false);
+
+  const editBtnHandler = e => {
+    e.preventDefault();
+    setModel(!showModel);
+  };
+
+  const closeBtnHandler = e => {
+    e.preventDefault();
+    setModel(!showModel);
+  };
+
+  return (
+    <div className="achievements-block">
+      <div className="card">
+        <CardHeader title="Achievements" />
+        <div className="achievements">
+          <div className="achievements__row">
+            <div className="achievements__row-header">
+              <div className="sub-title">Headhunt Award</div>
+              <EditOptions
+                component={AddAchievement}
+                onEdit={editBtnHandler}
+                onClose={closeBtnHandler}
+                showModel={showModel}
+              />
+            </div>
+            <div className="achievements__year">December 2012</div>
+          </div>
+        </div>
+        <CardFooter icon={Add} label="Add another achievement" />
+      </div>
+    </div>
+  );
+};
 
 export default Achievements;
--- src/components/form/achievement/AddAchievement.js
+++ src/components/form/achievement/AddAchievement.js
@@ -1,41 +1,69 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import { Formik, Form } from 'formik';
+import React, { useContext } from 'react';
+
 import { Close, Trash } from '~/assets/image';
 import Button from '~/components/button/Button';
+import { FormContext } from '../../FormContext';
 import InputText from '~/components/inputtext/InputText';
 import InputDate from '~/components/inputdate/InputDate';
 import FormHeader from '~/components/formheader/FormHeader';
-import InputTextArea from '~/components/inputtextarea/InputTextArea';
+import validateAchievementInformation from '~/validations/Achievement';
 
-const AddAchievement = () => {
+const AddAchievement = ({ onClose }) => {
+  const { data, setData } = useContext(FormContext);
+
   return (
     <div className="form-container">
       <div className="form">
         <div className="card">
-          <div className="close-option">
+          <div className="close-option" onClick={onClose}>
             <img src={Close} alt="close" />
           </div>
           <FormHeader title="Add Achievement" />
-          <div className="form__content">
-            <InputText label="Title of your Achievement" />
-            <InputDate label="Date of the Achievement" />
-            <InputText
-              label="Describe your Achievements (optional)"
-              placeholder="eg. I was awarded ..."
-              type="text-area"
-            />
-            <div className="form-button">
-              <div className="form-button__left">
-                <Button content="Add Achievement" />
-              </div>
-              <div className="form-button__right">
-                <Button content="Cancel" isCancel={true} />
-              </div>
-            </div>
-          </div>
+          <Formik
+            initialValues={{
+              name: '',
+              date: '',
+              description: '',
+            }}
+            onSubmit={values => {
+              setData(prevState => ({ ...prevState, ...values }));
+            }}
+            validationSchema={validateAchievementInformation}
+          >
+            {({ values }) => (
+              <Form>
+                <pre>{JSON.stringify(values, null, 2)}</pre>
+                <div className="form__content">
+                  <InputText name="name" label="Title of your Achievement" />
+                  <InputDate name="date" label="Date of the Achievement" />
+                  <InputText
+                    name="description"
+                    label="Describe your Achievements (optional)"
+                    placeholder="eg. I was awarded ..."
+                    type="text-area"
+                  />
+                  <div className="form-button">
+                    <div className="form-button__left">
+                      <Button content="Add Achievement" />
+                    </div>
+                    <div className="form-button__right">
+                      <Button content="Cancel" isCancel={true} />
+                    </div>
+                  </div>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </div>
   );
+};
+
+AddAchievement.propTypes = {
+  onClose: PropTypes.func,
 };
 
 export default AddAchievement;
