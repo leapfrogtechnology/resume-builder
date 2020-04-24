bd02b4ae228a1fb48c086715d9bde0260aba58e1
nikitashrestha <nikshres522@gmail.com>
2020-04-24 11:53:57 +0545

    modal-preview:#1 - add formik component with validation

--- src/components/certificate/Certificate.js
+++ src/components/certificate/Certificate.js
@@ -1,33 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Add, UpRightArrow } from '~/assets/image';
 import CardFooter from '~/components/cardfooter/CardFooter';
 import CardHeader from '~/components/cardheader/CardHeader';
 import EditOptions from '~/components/editoptions/EditOptions';
-import { Add, UpRightArrow } from '~/assets/image';
+import AddCertificate from '~/components/form/certificate/AddCertificate';
 
 const Certificate = () => {
-	return (
-		<div className="certificate-block">
-			<div className="card">
-				<CardHeader title="Certificates" />
-				<div className="certificate">
-					<div className="certificate__row">
-						<div className="certificate__row-header">
-							<div className="sub-title text-link">
-								Coursera Advanced React
-                <span className="arrow-icon"><img src={UpRightArrow} alt="Arrow" /></span>
-							</div>
-							<EditOptions />
-						</div>
-						<div className="certificate__year">December 2012</div>
-						<p className="certificate__description">
-							Advanced react course completed with React under the hood
-            </p>
-					</div>
-				</div>
-				<CardFooter icon={Add} label="Add another certificate" />
-			</div>
-		</div>
-	)
-}
+  const [showModel, setModel] = useState(false);
+
+  const editBtnHandler = e => {
+    e.preventDefault();
+    setModel(!showModel);
+  };
+
+  const closeBtnHandler = e => {
+    e.preventDefault();
+    setModel(!showModel);
+  };
+
+  return (
+    <div className="certificate-block">
+      <div className="card">
+        <CardHeader title="Certificates" />
+        <div className="certificate">
+          <div className="certificate__row">
+            <div className="certificate__row-header">
+              <div className="sub-title text-link">
+                Coursera Advanced React
+                <span className="arrow-icon">
+                  <img src={UpRightArrow} alt="Arrow" />
+                </span>
+              </div>
+              <EditOptions
+                component={AddCertificate}
+                onEdit={editBtnHandler}
+                onClose={closeBtnHandler}
+                showModal={showModel}
+              />
+            </div>
+            <div className="certificate__year">December 2012</div>
+            <p className="certificate__description">Advanced react course completed with React under the hood</p>
+          </div>
+        </div>
+        <CardFooter icon={Add} label="Add another certificate" />
+      </div>
+    </div>
+  );
+};
 
 export default Certificate;
--- src/components/form/certificate/AddCertificate.js
+++ src/components/form/certificate/AddCertificate.js
@@ -1,42 +1,71 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import { Formik, Form } from 'formik';
+import React, { useContext } from 'react';
+
 import { Close, Trash } from '~/assets/image';
+import { FormContext } from '../../FormContext';
 import Button from '~/components/button/Button';
 import InputText from '~/components/inputtext/InputText';
 import InputDate from '~/components/inputdate/InputDate';
 import FormHeader from '~/components/formheader/FormHeader';
-import InputTextArea from '~/components/inputtextarea/InputTextArea';
+import validateCertificateInformation from '~/validations/Certificate';
 
-const AddCertificate = () => {
+const AddCertificate = ({ onClose }) => {
+  const { data, setData } = useContext(FormContext);
+
   return (
     <div className="form-container">
       <div className="form">
         <div className="card">
-          <div className="close-option">
+          <div className="close-option" onClick={onClose}>
             <img src={Close} alt="close" />
           </div>
           <FormHeader title="Add Certificate" />
-          <div className="form__content">
-            <InputText label="Title of your Certificate" />
-            <InputText label="Link to this Certificate" placeholder="https://" />
-            <InputDate label="Date you received the Certificate" />
-            <InputText
-              label="Describe this Certificate (optional)"
-              placeholder="eg. I was awarded ..."
-              type="text-area"
-            />
-            <div className="form-button">
-              <div className="form-button__left">
-                <Button content="Add Certificate" />
-              </div>
-              <div className="form-button__right">
-                <Button content="Cancel" isCancel={true} />
-              </div>
-            </div>
-          </div>
+          <Formik
+            initialValues={{
+              name: '',
+              link: '',
+              date: '',
+              description: '',
+            }}
+            onSubmit={values => {
+              setData(prevState => ({ ...prevState, ...values }));
+            }}
+            validationSchema={validateCertificateInformation}
+          >
+            {({ values }) => (
+              <Form>
+                <div className="form__content">
+                  <pre>{JSON.stringify(values, null, 2)}</pre>
+                  <InputText name="name" label="Title of your Certificate" />
+                  <InputText name="link" label="Link to this Certificate" placeholder="https://" />
+                  <InputDate name="date" label="Date you received the Certificate" />
+                  <InputText
+                    name="description"
+                    label="Describe this Certificate (optional)"
+                    placeholder="eg. I was awarded ..."
+                    type="text-area"
+                  />
+                  <div className="form-button">
+                    <div className="form-button__left">
+                      <Button content="Add Certificate" />
+                    </div>
+                    <div className="form-button__right">
+                      <Button content="Cancel" isCancel={true} />
+                    </div>
+                  </div>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </div>
   );
+};
+
+AddCertificate.propTypes = {
+  onClose: PropTypes.func,
 };
 
 export default AddCertificate;
