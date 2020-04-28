e56f57aa2acb5acabc9bd5423c1915335ce77107
nikitashrestha <nikshres522@gmail.com>
2020-04-28 17:47:00 +0545

    modal-preview:#1 - add new line

--- src/utilities/resume/PdfGenerator.js
+++ src/utilities/resume/PdfGenerator.js
@@ -1,5 +1,6 @@
-export const downloadPDF = e => {
+export const downloadPDF = () => {
   var elementHTML = document.getElementById('pdf');
+
   html2canvas(elementHTML).then(canvas => {
     const imgData = canvas.toDataURL('image/png');
     const pdf = new jsPDF();
