95a547238f802f74b5cc0f6fc7323cf0feec5621
nikitashrestha <nikshres522@gmail.com>
2020-04-28 08:51:40 +0545

    pdf-generator:#3 - remove unused imports

--- src/components/pdf/Pdf.js
+++ src/components/pdf/Pdf.js
@@ -5,37 +5,31 @@ import Achievements from '~/components/achievements/Achievements';
 import CardHeader from '~/components/cardheader/CardHeader';
 import WorkExperience from '~/components/workexperience/WorkExperience';
 import ProjectsUndertaken from '~/components/projectsundertaken/ProjectsUndertaken';
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
-
 import ResumePersonalInformation from './ResumePersonalInformation';
 
 const Pdf = () => {
   return (
-    <Document>
-      <Page size="A4">
-        <div className="card" id="pdf">
-          <div className="resume-content-block">
-            <ResumePersonalInformation />
-          </div>
-          <div className="resume-content-block">
-            <CardHeader title="Skills" />
-            <p>PHP (Zend, Laravel, CI), Python (Django, Flash Alembic), Javascript (ReactJS, ReactNative)</p>
-          </div>
-          <div className="resume-content-block">
-            <WorkExperience />
-          </div>
-          <div className="resume-content-block">
-            <ProjectsUndertaken />
-          </div>
-          <div className="resume-content-block">
-            <Achievements />
-          </div>
-          <div className="resume-content-block">
-            <Certificate />
-          </div>
-        </div>
-      </Page>
-    </Document>
+    <div className="card" id="pdf">
+      <div className="resume-content-block">
+        <ResumePersonalInformation />
+      </div>
+      <div className="resume-content-block">
+        <CardHeader title="Skills" />
+        <p>PHP (Zend, Laravel, CI), Python (Django, Flash Alembic), Javascript (ReactJS, ReactNative)</p>
+      </div>
+      <div className="resume-content-block">
+        <WorkExperience />
+      </div>
+      <div className="resume-content-block">
+        <ProjectsUndertaken />
+      </div>
+      <div className="resume-content-block">
+        <Achievements />
+      </div>
+      <div className="resume-content-block">
+        <Certificate />
+      </div>
+    </div>
   );
 };
 
