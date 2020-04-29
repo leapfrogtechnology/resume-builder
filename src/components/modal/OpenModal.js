6ed2f91a2539b5a6a54d5f22cd001b9266332768
nikitashrestha <nikshres522@gmail.com>
2020-04-29 19:35:09 +0545

    modal-preview:#2 - add proptypes

--- src/components/modal/OpenModal.js
+++ src/components/modal/OpenModal.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Card from '~/components/card/Card';
 
-const OpenModal = ({ component, onClose, showModal }) => {
+const OpenModal = ({ component, onClose, showModal, isEdit }) => {
   const Component = component;
 
   return (
     <div className="modal-container">
       <div className="form">
-        <Card children={Component} onClose={onClose} showModal={showModal} />
+        <Card children={Component} onClose={onClose} showModal={showModal} isEdit={isEdit} />
       </div>
     </div>
   );
+};
+
+OpenModal.propTypes = {
+  component: PropTypes.func,
+  onClose: PropTypes.func,
+  showModal: PropTypes.bool,
+  isEdit: PropTypes.bool,
 };
 
 export default OpenModal;
