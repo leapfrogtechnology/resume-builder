d1b6c90e7a9231cb986fff7ff454f2f926f9a41f
nikitashrestha <nikshres522@gmail.com>
2020-04-29 08:40:10 +0545

    modal-preview:#2 - add onClick handler to contact value

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { View, ViewHidden } from '~/assets/image';
 
-const Contact = ({ id, label, value, preview, onHiddenIconClicked }) => {
+const Contact = ({ id, label, value, preview, onHiddenIconClicked, onLinkClicked }) => {
   const [hidden, setHidden] = useState(false);
 
   const onHiddenBtnClicked = e => {
     e.preventDefault();
     setHidden(!hidden);
     onHiddenIconClicked(e, id);
+  };
+
+  const handleOnLinkClicked = (e, value) => {
+    e.preventDefault();
+    onLinkClicked(e, value);
   };
 
   if (!value || (preview && hidden)) {
@@ -23,7 +27,14 @@ const Contact = ({ id, label, value, preview, onHiddenIconClicked }) => {
           {label}
           {hidden && !preview && <span className="hidden-tag">Hidden</span>}
         </div>
-        <div className="value text-link">{value}</div>
+        <div
+          className="value text-link"
+          onClick={e => {
+            handleOnLinkClicked(e, value);
+          }}
+        >
+          {value}
+        </div>
       </div>
       {!preview && (
         <div className="contact-content__r" onClick={e => onHiddenBtnClicked(e)}>
