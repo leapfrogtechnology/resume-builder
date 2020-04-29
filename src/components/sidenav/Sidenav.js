999f143582612e8df2f1180bb2f0fc6e79214feb
nikitashrestha <nikshres522@gmail.com>
2020-04-29 20:09:07 +0545

    modal-preview:#2 - add isEdit state

--- src/components/sidenav/Sidenav.js
+++ src/components/sidenav/Sidenav.js
@@ -1,25 +1,28 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 
+import SidenavBottom from './SidenavBottom';
 import { FormContext } from '../FormContext';
 import Contact from '~/components/contact/Contact';
 import { toBase64 } from '~/utilities/file/toBase64.js';
+import { Edit, ProfileImage, Trash } from '~/assets/image';
 import CardHeader from '~/components/cardheader/CardHeader';
 import AddContact from '~/components/form/contact/AddContact';
-import * as pdfGenerator from '~/utilities/resume/PdfGenerator.js';
 import * as profileImageUtils from '~/utilities/objects/ProfileImage.js';
-import { Edit, ProfileImage, Trash, Download, Copy, Email, Check, Delete } from '~/assets/image';
 
 const Sidenav = () => {
   const [showModal, setModal] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
 
   const editBtnHandler = e => {
     e.preventDefault();
     setModal(!showModal);
+    setIsEdit(!isEdit);
   };
 
   const closeBtnHandler = e => {
     e.preventDefault();
     setModal(!showModal);
+    setIsEdit(!isEdit);
   };
 
   const context = useContext(FormContext);
@@ -125,6 +128,7 @@ const Sidenav = () => {
               onEdit={editBtnHandler}
               onClose={closeBtnHandler}
               showModal={showModal}
+              isEdit={isEdit}
             />
             {email && (
               <Contact
@@ -180,47 +184,7 @@ const Sidenav = () => {
         </div>
       </div>
 
-      {!preview && (
-        <div className="sidenav-bottom">
-          <div className="card">
-            <CardHeader title="CV Actions" />
-            <ul>
-              <li className="sidenav__cv-action">
-                <span className="sidenav__cv-action-icon">
-                  <img src={Download} alt="Edit" />
-                </span>
-                <span className="sidenav__cv-action-label text-link" onClick={e => pdfGenerator.downloadPDF(e)}>
-                  Download PDF
-                </span>
-              </li>
-              <li className="sidenav__cv-action">
-                <span className="sidenav__cv-action-icon">
-                  <img src={Copy} alt="Edit" />
-                </span>
-                <span className="sidenav__cv-action-label text-link">Copy Shareable Link</span>
-              </li>
-              <li className="sidenav__cv-action">
-                <span className="sidenav__cv-action-icon">
-                  <img src={Email} alt="Edit" />
-                </span>
-                <span className="sidenav__cv-action-label text-link">Email CV as Attachment</span>
-              </li>
-              <li className="sidenav__cv-action">
-                <span className="sidenav__cv-action-icon">
-                  <img src={Check} alt="Edit" />
-                </span>
-                <span className="sidenav__cv-action-label text-link">Request Professional Review</span>
-              </li>
-              <li className="sidenav__cv-action">
-                <span className="sidenav__cv-action-icon">
-                  <img src={Delete} alt="Edit" />
-                </span>
-                <span className="sidenav__cv-action-label text-link text-link--danger">Delete CV</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-      )}
+      {!preview && <SidenavBottom />}
     </div>
   );
 };
