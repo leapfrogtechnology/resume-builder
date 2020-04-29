93e916c9e81da2d4efa1b5d4063376364a4256ff
nikitashrestha <nikshres522@gmail.com>
2020-04-29 12:05:22 +0545

    modal-preview:#1 - add utility to convert image to base64

---
+++ src/utilities/file/toBase64.js
@@ -0,0 +1,8 @@
+export const toBase64 = file => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+};
