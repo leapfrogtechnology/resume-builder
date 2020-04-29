b8acba6de15001a82975e2d99735a8213f94e0e5
nikitashrestha <nikshres522@gmail.com>
2020-04-29 19:22:34 +0545

    modal-preview:#1 - add isEdit props and define proptypes

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import CloseModal from '../modalclose/CloseModal';
 
-const Card = ({ children, className = '', onClose, showModal }) => {
+const Card = ({ children, className = '', onClose, showModal, isEdit }) => {
   const Component = children;
 
   return (
     <div className={`card ${className}`}>
       {showModal && <CloseModal onClose={onClose} />}
-      <Component onClose={onClose} />
+      <Component onClose={onClose} isEdit={isEdit} />
     </div>
   );
+};
+
+Card.propTypes = {
+  children: PropTypes.func,
+  className: PropTypes.string,
+  onClose: PropTypes.func,
+  showModal: PropTypes.bool,
+  isEdit: PropTypes.bool,
 };
 
 export default Card;
