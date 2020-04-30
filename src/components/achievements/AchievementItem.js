c546acf920a8d95c6883b2af1938d9071ca7cbbe
nikitashrestha <nikshres522@gmail.com>
2020-04-30 11:22:07 +0545

    modal-preview:#2 - add OpenModal component and propTypes

--- src/components/achievements/AchievementItem.js
+++ src/components/achievements/AchievementItem.js
@@ -1,8 +1,11 @@
 import ProptTypes from 'prop-types';
 import React, { useState } from 'react';
-import EditOptions from '~/components/editoptions/EditOptions';
 
-const AchievementItem = ({ title, date, preview, onHiddenIconClicked, onEdit }) => {
+import OpenModal from '../modal/OpenModal';
+import EditOptions from '~/components/editoptions/EditOptions';
+import AddAchievement from '~/components/form/achievement/AddAchievement';
+
+const AchievementItem = ({ title, date, description, preview, isEdit, onHiddenIconClicked, onEdit, onClose }) => {
   const [hidden, setHidden] = useState(false);
 
   if (hidden && preview) {
@@ -25,6 +28,15 @@ const AchievementItem = ({ title, date, preview, onHiddenIconClicked, onEdit }) 
         {!preview && (
           <EditOptions isHidden={hidden} onHiddenIconClicked={onHiddenBtnClicked} onEditButtonClicked={onEdit} />
         )}
+        {isEdit && (
+          <OpenModal
+            component={AddAchievement}
+            onClose={onClose}
+            showModal={isEdit}
+            isEdit={isEdit}
+            data={isEdit ? { name: title, date: date, description: description } : ''}
+          ></OpenModal>
+        )}
       </div>
       <div className="year year--dark">{moment(date).format('MMMM YYYY')}</div>
     </div>
@@ -34,9 +46,12 @@ const AchievementItem = ({ title, date, preview, onHiddenIconClicked, onEdit }) 
 AchievementItem.propTypes = {
   title: ProptTypes.string,
   date: ProptTypes.string,
+  description: ProptTypes.string,
   preview: ProptTypes.bool,
+  isEdit: ProptTypes.bool,
   onHiddenIconClicked: ProptTypes.func,
   onEdit: ProptTypes.func,
+  onClose: ProptTypes.func,
 };
 
 export default AchievementItem;
