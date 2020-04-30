aa8718b31cf05cce142c7e0008f093d47a0cad49
nikitashrestha <nikshres522@gmail.com>
2020-04-30 20:06:56 +0545

    modal-preview : #2 add delete props

--- src/components/editoptions/EditOptions.js
+++ src/components/editoptions/EditOptions.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import OpenModal from '~/components/modal/OpenModal';
-import { View, EditGray, Trash, ViewHidden } from '~/assets/image';
-import DeletePopup from '../form/delete/DeletePopup';
 
-const EditOptions = ({
-  isHidden = false,
-  onHiddenIconClicked,
-  onEditButtonClicked,
-  onDeleteButtonClicked,
-  isEditClicked,
-  isDeleteClicked,
-  onConfirm,
-  onCancel,
-}) => {
+import { View, EditGray, Trash, ViewHidden } from '~/assets/image';
+
+const EditOptions = ({ isHidden = false, onHiddenIconClicked, onEditButtonClicked, onDeleteButtonClicked }) => {
   return (
     <>
       <div className="edit-options">
@@ -26,7 +16,6 @@ const EditOptions = ({
         <span className="edit-options__item" onClick={e => onDeleteButtonClicked(e)}>
           <img src={Trash} alt="Trash" />
         </span>
-        {isDeleteClicked ? <DeletePopup onConfirm={onConfirm} onCancel={onCancel} /> : <></>}
       </div>
     </>
   );
@@ -34,12 +23,9 @@ const EditOptions = ({
 
 EditOptions.propTypes = {
   isHidden: PropTypes.bool,
-  showModal: PropTypes.bool,
-  onEdit: PropTypes.func,
-  onClose: PropTypes.func,
-  onDelete: PropTypes.func,
-  component: PropTypes.func,
   onHiddenIconClicked: PropTypes.func,
+  onEditButtonClicked: PropTypes.func,
+  onDeleteButtonClicked: PropTypes.func,
 };
 
 export default EditOptions;
