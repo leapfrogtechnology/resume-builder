681fe07cc514b37302cffc57f5eaa24d884556ff
nikitashrestha <nikshres522@gmail.com>
2020-04-30 13:26:43 +0545

    modal-preview:#2 - add edit handler

--- src/components/form/skill/AddSkill.js
+++ src/components/form/skill/AddSkill.js
@@ -1,8 +1,9 @@
 /* eslint-disable require-jsdoc */
 /* eslint-disable react/prop-types */
 import * as Yup from 'yup';
-import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import { Formik, Form } from 'formik';
+import React, { useContext } from 'react';
 
 import Button from '~/components/button/Button';
 import { FormContext } from '../../FormContext';
@@ -11,15 +12,27 @@ import FormHeader from '~/components/formheader/FormHeader';
 import InputSelect from '~/components/inputselect/InputSelect';
 import * as skillUtils from '../../../utilities/objects/Skill';
 
-const AddSkill = ({ onClose }) => {
+const AddSkill = ({ onClose, isEdit, values }) => {
   const { preview, data } = useContext(FormContext);
 
   const validateSkill = Yup.object().shape({
     skill: Yup.string().required(),
   });
 
-  const submitHandler = values => {
-    const skillObj = skillUtils.getSkillObject({ ...values });
+  let initialValues = {};
+
+  const handleSubmit = formValues => {
+    if (isEdit) {
+      handleSubmitOnEdit(formValues);
+    } else {
+      handleSubmitOnAdd(formValues);
+    }
+
+    data.set(prevState => ({ ...prevState, ...data }));
+  };
+
+  const handleSubmitOnAdd = formValues => {
+    const skillObj = skillUtils.getSkillObject({ ...formValues });
 
     if (data.get.skills) {
       data.get['skills'].push(skillObj);
@@ -27,22 +40,54 @@ const AddSkill = ({ onClose }) => {
       data.get['skills'] = [];
       data.get['skills'].push(skillObj);
     }
+  };
 
-    data.set(prevState => ({ ...prevState, ...data }));
+  const handleSubmitOnEdit = formValues => {
+    const isEqual = _.isEqual(formValues, initialValues);
+
+    if (isEqual) {
+      return;
+    } else {
+      const skillObj = skillUtils.getSkillObject({ ...formValues });
+      const skills = data.get['skills'];
+
+      const index = skills.findIndex(skill => {
+        return skill.name === values.name;
+      });
+
+      data.get['skills'][index] = skillObj;
+    }
+  };
+
+  const getInitialValues = () => {
+    if (isEdit) {
+      initialValues = {
+        skill: values.name,
+        subSkills: values.subSkills
+          .map(subSkill => {
+            return subSkill.name;
+          })
+          .join(),
+      };
+    } else {
+      initialValues = {
+        skill: '',
+        subSkills: '',
+      };
+    }
+    return initialValues;
   };
 
   return (
     <>
-      <FormHeader title="Add Skill" />
+      <FormHeader title={!isEdit ? 'Add Skill' : 'Edit Skill'} />
       <Formik
-        initialValues={{
-          skill: '',
-          subSkills: '',
-        }}
+        initialValues={getInitialValues()}
         onSubmit={values => {
-          submitHandler(values);
+          handleSubmit(values);
         }}
         validationSchema={validateSkill}
+        validateOnChange={validateSkill}
       >
         {({ values }) => (
           <Form>
@@ -64,6 +109,12 @@ const AddSkill = ({ onClose }) => {
       </Formik>
     </>
   );
+};
+
+AddSkill.propTypes = {
+  onClose: PropTypes.func,
+  isEdit: PropTypes.bool,
+  values: PropTypes.object || PropTypes.string,
 };
 
 export default AddSkill;
