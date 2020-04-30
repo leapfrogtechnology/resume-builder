bcd80e92466231d3d8a8282008c989bfae89d511
nikitashrestha <nikshres522@gmail.com>
2020-04-30 20:11:15 +0545

    modal-preview : #2 add onclick props

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Edit } from '~/assets/image';
 
-const Media = ({ icon, label }) => {
+const Media = ({ icon, label, onclick }) => {
   return (
     <div className="media">
       <span className="media__icon">{icon}</span>
       <span className="media__body">{label}</span>
+      <div className="media__body" onClick={onclick}>
+        <img src={Edit} alt="Edit" />
+      </div>
     </div>
   );
 };
@@ -13,6 +17,7 @@ const Media = ({ icon, label }) => {
 Media.propTypes = {
   icon: PropTypes.object,
   label: PropTypes.string,
+  onclick: PropTypes.func,
 };
 
 export default Media;
