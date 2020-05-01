71e09977be21470f6e7a4e0b4328858ef2a0b32d
nikitashrestha <nikshres522@gmail.com>
2020-05-01 10:01:24 +0545

    modal-preview:#1 - add setFieldValue and setFieldTouched props, use DatePicker component

--- src/components/inputdate/InputDate.js
+++ src/components/inputdate/InputDate.js
@@ -1,18 +1,45 @@
-import React from 'react';
 import { useField } from 'formik';
+import React, { useState } from 'react';
 
 import PropTypes from 'prop-types';
 import { Calendar } from '~/assets/image';
+import DatePicker from 'react-datepicker';
 
-const InputDate = ({ label, placeholder, modifier, setFieldValue, ...props }) => {
+const InputDate = ({ label, placeholder, modifier, setFieldValue, setFieldTouched, ...props }) => {
   const [field, meta] = useField(props);
+
+  const [startDate, setDate] = useState();
+  const [datePickerIsOpen, setDatePicker] = useState(false);
+
+  const handleChange = date => {
+    setFieldTouched(field.name, true);
+    setFieldValue(field.name, date, true);
+    setDate(date);
+    setDatePicker(!datePickerIsOpen);
+  };
+
+  const openDatePicker = () => {
+    setDatePicker(!datePickerIsOpen);
+  };
 
   return (
     <div className={modifier ? 'input--short' : 'input'}>
       <label className="input__label">{label}</label>
       <div className={'input__date'}>
-        <input type="date" className="input__date-field" placeholder={placeholder} {...field} />
-        <img src={Calendar} alt="Calendar" />
+        <DatePicker
+          name={field.name}
+          selected={startDate}
+          placeholderText={placeholder}
+          shouldCloseOnSelect={true}
+          dateFormat="MM/dd/yyyy"
+          onChange={handleChange}
+          onInputClick={openDatePicker}
+          onClickOutside={e => {
+            setDatePicker(false);
+          }}
+          open={datePickerIsOpen}
+        ></DatePicker>
+        <img src={Calendar} alt="Calendar" onClick={openDatePicker} />
       </div>
       {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
     </div>
@@ -23,6 +50,8 @@ InputDate.propTypes = {
   label: PropTypes.string,
   placeholder: PropTypes.string,
   modifier: PropTypes.bool,
+  setFieldValue: PropTypes.func,
+  setFieldTouched: PropTypes.func,
 };
 
 export default InputDate;
