2aaec9ff72aa178ca1f512b1bcac57a4e4f6c149
nikitashrestha <nikshres522@gmail.com>
2020-05-04 09:16:43 +0545

    modal-preview:#4 - add regex for checking links

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,7 +2,7 @@ const skills = ['JS', 'Python', 'Ruby', 'Java'];
 
 const emailCheck = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const phoneNumberCheck = /^(\+91-|\+91|0)?\d{10}$/;
+const phoneNumberCheck = /^(\+91-|\+91|\+|\+97|0)?\d{10}$/;
 
 const linkCheck = new RegExp(
   '^(https?:\\/\\/)?' + // protocol
