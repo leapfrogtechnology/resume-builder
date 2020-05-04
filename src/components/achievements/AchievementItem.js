82ce1525e45250713c093e0970380f529ac0227c
nikitashrestha <nikshres522@gmail.com>
2020-05-04 13:56:52 +0545

    modal-preview:#1 - add conditional styling based on hidden state

--- src/components/achievements/AchievementItem.js
+++ src/components/achievements/AchievementItem.js
@@ -35,7 +35,7 @@ const AchievementItem = ({ title, date, description, preview, onHiddenIconClicke
   };
 
   return (
-    <div className="achievements__row">
+    <div className={!hidden ? 'achievements__row' : 'achievements__row achievements--hidden'}>
       <div className="achievements__row-header">
         <div className="sub-title">
           {title}
