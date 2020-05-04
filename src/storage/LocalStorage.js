0d8e1e15e1c088dbfa9008da97cf4ea1d998e42f
nikitashrestha <nikshres522@gmail.com>
2020-05-04 09:37:30 +0545

    modal-preview:#4 - add delete resume method from local storage

--- src/storage/LocalStorage.js
+++ src/storage/LocalStorage.js
@@ -5,3 +5,7 @@ export const getResume = storage => {
 export const saveResume = (storage, resume) => {
   storage.setItem('resume', JSON.stringify(resume));
 };
+
+export const deleteResume = storage => {
+  storage.clear();
+};
