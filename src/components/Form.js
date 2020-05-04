9156f6e891e18a4cffa5be171423cb3d162816c8
nikitashrestha <nikshres522@gmail.com>
2020-05-04 09:35:43 +0545

    modal-preview:#4 - add edit proptypes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Card from './card/Card';
 
-const Form = ({ children }) => {
-  
+const Form = ({ component }) => {
   return (
     <div className="form-container">
       <div className="form">
-        <Card children={children} />
+        <Card children={component} />
       </div>
     </div>
   );
-}
+};
+
+Form.propTypes = {
+  component: PropTypes.func,
+};
 
 export default Form;
