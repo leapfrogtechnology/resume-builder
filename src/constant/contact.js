f500412647150e55a16c15b77f55c418327a640c
nikitashrestha <nikshres522@gmail.com>
2020-05-05 20:09:48 +0545

    modal-preview:#1 - add baseUrl

--- src/constant/contact.js
+++ src/constant/contact.js
@@ -1,3 +1,6 @@
 export const COUNTRY_CODE = '+977';
 export const BASE_URL_LINKED_IN = 'https://www.linkedin.com/in/';
 export const BASE_URL_GITHUB = 'https://github.com/';
+
+export const baseTelUrl = 'tel:';
+export const baseMailToUrl = 'https://mail.google.com/mail/?view=cm&fs=1&to=';
