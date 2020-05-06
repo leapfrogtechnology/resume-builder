450fc0ecf0c921df36a5b0e57074edc5dca90003
nikitashrestha <nikshres522@gmail.com>
2020-05-06 17:58:49 +0545

    pdf-generator:#3 - add personal info section

--- src/utilities/resume/pdf.js
+++ src/utilities/resume/pdf.js
@@ -1,101 +1,93 @@
+import { useState, useContext } from 'react';
 import { Page, Text, View, Document, StyleSheet, Image, Font } from '@react-pdf/renderer';
 import { ProfileImage } from '~/assets/image';
+import { FormContext } from '../../components/FormContext';
 
-// Create styles
+// Create styles for overall page
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'row',
     backgroundColor: '#E4E4E4',
     margin: '60px 32px',
   },
-  personalInformation: {
-    display: 'flex',
-  },
-  infoLeft: {
-    marginRight: '20px',
-  },
-  infoRight: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  profileImageWrapper: {
-    marginRight: '20px',
-    width: '50px',
-    height: '50px',
-    borderRadius: '50%',
-    overflow: 'hidden',
-  },
 });
 
+// Create styles for personal information section
 const personalInformationStyles = StyleSheet.create({
   personalInformation: {
-    display: ' -webkit-flex' /* Safari */,
     display: 'flex',
+    flexDirection: 'row',
   },
+
   left: {
-    marginRight: '20px',
-    flex: 1,
+    marginRight: 20,
   },
-  right: {
-    flex: 1,
-  },
+
   profileImageWrapper: {
-    marginRight: '20px',
-    width: '50px',
-    height: '50px',
+    marginRight: 20,
+    width: 50,
+    height: 50,
     borderRadius: '50%',
     overflow: 'hidden',
   },
   profileDetail: {
-    display: 'block',
+    display: 'inline-block',
   },
   profileName: {
-    // fontStyle: 'normal',
-    fontSize: 12,
+    fontSize: 28,
     fontWeight: 'bold',
   },
   profileStatus: {
-    fontStyle: 'normal',
-    fontSize: 12,
+    fontWeight: 'normal',
+    fontSize: 12 || 14,
   },
   content: {
-    marginTop: '24px',
+    marginTop: 24,
   },
   contentRight: {
-    // fontStyle: 'normal',
-    // // fontFamily: 'sans-serif',
-    fontSize: 12,
     fontWeight: 'normal',
+    fontSize: 12 || 14,
   },
   contentLeft: {
-    width: '50px',
-    // fontStyle: 'normal',
-    // // fontFamily: 'sans-serif',
-    fontSize: 12,
+    width: 50,
+    fontSize: 12 || 14,
     fontWeight: 'bold',
-    marginRight: '20px',
+    marginRight: 20,
   },
   row: {
     display: 'block',
-    // fontStyle: 'normal',
     fontSize: 12,
   },
   rowItem: {
     display: 'flex',
-    // textAlign: '-webkit-match-parent',
+    flexDirection: 'row',
   },
 });
 
 // Create Document Component
-const MyDocument = () => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <PersonalInformation></PersonalInformation>
-    </Page>
-  </Document>
-);
+const MyDocument = resumeJson => {
+  //This data shall be removed later
+  const contactsList = [{ label: 'Email', value: 'nikitashrestha@lftechnology.com' }];
+  const name = 'Nikita Shrestha';
+  const role = { label: 'Asociate Software Engineer', hidden: false };
 
-const PersonalInformation = () => {
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <PersonalInformation name={name} role={role} contactsList={contactsList}></PersonalInformation>
+      </Page>
+    </Document>
+  );
+};
+
+/**
+ * Create personal info section.
+ *
+ * @param {string} name Name of resumee holder.
+ * @param {object} role Role of resume holder.
+ * @param {Array} contactsList List of contacts having hidden = false.
+ */
+const PersonalInformation = ({ name, role, contactsList }) => {
   return (
     <View style={personalInformationStyles.personalInformation}>
       <View style={personalInformationStyles.left}>
@@ -103,13 +95,22 @@ const PersonalInformation = () => {
           <Image src={ProfileImage} alt="Image"></Image>
         </View>
       </View>
-      <View>
+      <View style={personalInformationStyles.right}>
         <View>
-          <Text>ergt</Text>
+          <View>
+            <Text style={[personalInformationStyles.profileName]}>{name}</Text>
+          </View>
+          {role && !role.hidden && (
+            <View>
+              <Text style={personalInformationStyles.profileStatus}>{role.label}</Text>
+            </View>
+          )}
         </View>
         <View style={personalInformationStyles.content}>
           <View style={personalInformationStyles.row}>
-            <PersonalInformationItem label="Email" value="nikitashresth@lftechnology.com"></PersonalInformationItem>
+            {contactsList.map((contact, index) => (
+              <PersonalInformationItem label={contact.label} value={contact.value} key={index} />
+            ))}
           </View>
         </View>
       </View>
