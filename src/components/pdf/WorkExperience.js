e92ed16af4a29be48590aa5f658542a27a42dee0
nikitashrestha <nikshres522@gmail.com>
2020-05-08 16:03:46 +0545

    pdf-generator:#3 - add WorkExperience component file

---
+++ src/components/pdf/WorkExperience.js
@@ -0,0 +1,55 @@
+import { Text, View } from '@react-pdf/renderer';
+import * as dateUtils from '~/utilities/date/FormatDate';
+import * as pdfStyles from '~/components/pdf/pdf.styles.js';
+import WorkExperienceItem from '~/components/pdf/WorkExperienceItem';
+
+/**
+ * Create work experience section.
+ *
+ * @param {string} heading Header to show.
+ * @param {object} experience Overall experience of resumee holder.
+ * @param {Array} data Array of workExperience object.
+ */
+const WorkExperience = ({ heading, experience, data }) => {
+  let experienceLabel = '';
+
+  //Create label to show overall work experience.
+  if (experience) {
+    const experienceInYearAndMonth = dateUtils.getExperienceFormat(experience);
+
+    experienceLabel = '';
+
+    if (experienceInYearAndMonth.year === 0 && experienceInYearAndMonth.month === 0) {
+      experienceLabel = '';
+    } else {
+      if (experienceInYearAndMonth.year !== 0) {
+        experienceLabel =
+          experienceInYearAndMonth.year > 1
+            ? `${experienceInYearAndMonth.year} years `
+            : `${experienceInYearAndMonth.year}  year `;
+      }
+
+      if (experienceInYearAndMonth.month !== 0) {
+        experienceLabel +=
+          experienceInYearAndMonth.month > 1
+            ? `${experienceInYearAndMonth.month} months `
+            : `${experienceInYearAndMonth.month} month `;
+      }
+    }
+  }
+
+  experienceLabel = experienceLabel ? `( ${experienceLabel} )` : '';
+
+  const workExperiences = data.map((value, index) => (
+    <WorkExperienceItem key={index} workExperience={value}></WorkExperienceItem>
+  ));
+
+  return (
+    <View style={pdfStyles.styles.resumeContentBlock}>
+      <Text style={pdfStyles.styles.contentHeader}>{`${heading} ${experienceLabel}`}</Text>
+      {workExperiences}
+    </View>
+  );
+};
+
+export default WorkExperience;
