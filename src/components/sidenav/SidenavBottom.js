a01cd22055264198a980e6c9afe7197a02a6ec55
nikitashrestha <nikshres522@gmail.com>
2020-05-08 19:10:12 +0545

    design-fix:#2 - remove handler for download pdf

--- src/components/sidenav/SidenavBottom.js
+++ src/components/sidenav/SidenavBottom.js
@@ -1,58 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import resumeDoc from '~/utilities/resume/word.js';
 import CardHeader from '~/components/cardheader/CardHeader';
-import { PDFDownloadLink } from '@react-pdf/renderer';
-import MyDocument from '~/utilities/resume/pdf';
 import { DELETE, CHECK, EMAIL, COPY_LINK, DOWNLOAD } from '~/components/icons/icon';
 
-const SidenavBottom = ({ resumeJson, deleteIconClicked }) => {
+const SidenavBottom = ({ deleteIconClicked }) => {
   return (
     <div className="sidenav-bottom">
       <div className="card">
         <CardHeader title="CV Actions" />
         <ul>
-          <li
-            className="sidenav__cv-action"
-            onClick={e => {
-              e.preventDefault();
-              resumeDoc(resumeJson);
-            }}
-          >
-            <span className="sidenav__cv-action-icon">
-              {DOWNLOAD}
-            </span>
+          <li className="sidenav__cv-action">
+            <span className="sidenav__cv-action-icon">{DOWNLOAD}</span>
             <span className="sidenav__cv-action-label text-link">Download PDF</span>
           </li>
           <li className="sidenav__cv-action">
-            <span className="sidenav__cv-action-icon">
-              {COPY_LINK}
-            </span>
+            <span className="sidenav__cv-action-icon">{COPY_LINK}</span>
             <span className="sidenav__cv-action-label text-link">Copy Shareable Link</span>
           </li>
           <li className="sidenav__cv-action">
-            <span className="sidenav__cv-action-icon">
-              {EMAIL}
-            </span>
+            <span className="sidenav__cv-action-icon">{EMAIL}</span>
             <span className="sidenav__cv-action-label text-link">Email CV as Attachment</span>
           </li>
           <li className="sidenav__cv-action">
-            <span className="sidenav__cv-action-icon">
-              {CHECK}
-            </span>
+            <span className="sidenav__cv-action-icon">{CHECK}</span>
             <span className="sidenav__cv-action-label text-link">Request Professional Review</span>
           </li>
           <li className="sidenav__cv-action" onClick={deleteIconClicked}>
-            <span className="sidenav__cv-action-icon">
-              {DELETE('#F44336')}
-            </span>
+            <span className="sidenav__cv-action-icon">{DELETE('#F44336')}</span>
             <span className="sidenav__cv-action-label text-link text-link--danger">Delete CV</span>
-          </li>
-          <li>
-            <PDFDownloadLink document={<MyDocument />} fileName="somename.pdf">
-              {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
-            </PDFDownloadLink>
           </li>
         </ul>
       </div>
