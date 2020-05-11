167b7ceb0703b98724290e0ab69642e3ad0b7072
nikitashrestha <nikshres522@gmail.com>
2020-05-11 13:34:41 +0545

    design-fix:#2 - remove edit icon on preview

--- src/components/userdetail/UserDetail.js
+++ src/components/userdetail/UserDetail.js
@@ -23,7 +23,7 @@ const UserDetail = ({ name, experience, profileImg, preview, onPreviewBtnClicked
           <div className="user-detail__emp-attribute">
             <div className="user-detail__username">{name ? name : 'Welcome, Leapfrogger !'}</div>
             <div className="user-detail__activity">
-              <Media icon={FAVORITE_ICON} label={experience} onclick={toggleEdit} />
+              <Media icon={FAVORITE_ICON} label={experience} preview={preview} onclick={toggleEdit} />
               {showModal && (
                 <OpenModal component={AddExperience} onClose={toggleEdit} showModal={showModal}></OpenModal>
               )}
