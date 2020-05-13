4620c5edd41597da113fddc2db260480a8f969f7
nikitashrestha <nikshres522@gmail.com>
2020-05-13 15:25:17 +0545

    dev - add utility to ensure url has protocol

---
+++ src/utilities/string/checkProtocol.js
@@ -0,0 +1,12 @@
+import { protocolRegEx, http } from '~/common/constants';
+
+/**
+ * Ensure https protocol exist in link.
+ *
+ * @param {string} url
+ */
+export const ensureHasProtocol = url => {
+  if (!protocolRegEx.test(url)) return http + url;
+
+  return url;
+};
