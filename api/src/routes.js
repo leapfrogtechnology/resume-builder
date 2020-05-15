6bae8f55923e1a524a6c93737d18572463867ad6
nikitashrestha <nikshres522@gmail.com>
2020-05-15 17:31:14 +0545

    login:#11 - add auth routes

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,30 +1,12 @@
 import { Router } from 'express';
 
-import swaggerSpec from './utils/swagger';
-import userRoutes from './routes/userRoutes';
+import authController from './controllers/auth';
 
 /**
  * Contains all API routes for the application.
  */
 const router = Router();
 
-/**
- * GET /api/swagger.json
- */
-router.get('/swagger.json', (req, res) => {
-  res.json(swaggerSpec);
-});
-
-/**
- * GET /api
- */
-router.get('/', (req, res) => {
-  res.json({
-    app: req.app.locals.title,
-    apiVersion: req.app.locals.version,
-  });
-});
-
-router.use('/users', userRoutes);
+router.use('/', authController);
 
 export default router;
