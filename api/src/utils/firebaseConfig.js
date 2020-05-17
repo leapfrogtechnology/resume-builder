95c98d4b59f5b9fb3cdf969542b019b42b1a75f6
nikitashrestha <nikshres522@gmail.com>
2020-05-17 13:50:12 +0545

    login:#11 - add firebase config

---
+++ api/src/utils/firebaseConfig.js
@@ -0,0 +1,25 @@
+const env = require('./../env');
+const firebase = require('firebase');
+const admin = require('firebase-admin');
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_API_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+var serviceAccount = require(process.env.FIREBASE_ADMIN_SDK_FILE);
+
+firebase.initializeApp(firebaseConfig);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
+module.exports = { firebase, admin };
