f6b0df4fdd8d58d160a311bd79492c15360534a0
nikitashrestha <nikshres522@gmail.com>
2020-05-17 14:06:48 +0545

    login:#11 - add method to store and retrive user and token

--- src/storage/LocalStorage.js
+++ src/storage/LocalStorage.js
@@ -1,4 +1,4 @@
-import { RESUME } from '~/constant/storage.js';
+import { RESUME, USER, ACCESS_TOKEN } from '~/constant/storage.js';
 
 export const getResume = () => {
   return JSON.parse(localStorage.getItem(RESUME));
@@ -11,3 +11,29 @@ export const saveResume = resume => {
 export const deleteResume = () => {
   localStorage.clear();
 };
+
+export const getUser = () => {
+  return JSON.parse(localStorage.getItem(USER));
+};
+
+export const saveUser = user => {
+  localStorage.setItem(USER, JSON.stringify(user));
+};
+
+export const getAccessToken = () => {
+  return JSON.parse(localStorage.getItem(ACCESS_TOKEN));
+};
+
+export const saveAccessToken = token => {
+  return localStorage.setItem(ACCESS_TOKEN, token);
+};
+
+/**
+ * Logout User and Remove Data from Browser Storage.
+ *
+ */
+export function logout() {
+  localStorage.removeItem(ACCESS_TOKEN);
+  localStorage.removeItem(USER);
+  window.location.href = routeConstants.LOGIN;
+}
