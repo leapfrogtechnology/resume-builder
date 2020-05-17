0d14f620174f79b571283b1cb61cba567a8166ca
nikitashrestha <nikshres522@gmail.com>
2020-05-17 14:05:53 +0545

    login:#11 - use Router component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 import Head from 'next/head';
-import GoogleLoginComponent from '../components/login/Login';
-import Header from '~/components/header/Header';
-import * as storage from '~/storage/LocalStorage';
-import { FormContext } from '~/components/FormContext';
-import Dashboard from '~/components/dashboard/Dashboard';
+import Router from '~/routes';
 
 const App = () => {
-  // App state
-  const [preview, setPreview] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [data, updateData] = useState({});
-
-  const username = data.name;
-
-  const togglePreview = () => setPreview(!preview);
-
-  const deleteCVHandler = () => {
-    storage.deleteResume();
-    updateData({});
-  };
-
-  const store = {
-    preview: { get: preview, set: setPreview },
-    data: { get: data, set: updateData },
-    deleteCV: deleteCVHandler,
-  };
 
   useEffect(() => {
-    const resume = storage.getResume();
-
-    if (resume) {
-      updateData(prevState => ({ ...prevState, ...resume }));
-    } else {
-      updateData({});
-    }
     setLoading(!loading);
   }, []);
 
@@ -52,11 +23,7 @@ const App = () => {
         <title>ResumeBuilder</title>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.js"></script>
       </Head>
-      <FormContext.Provider value={store}>
-        <Header name={username} status="Employee" onPreviewBtnClicked={togglePreview} />
-        <Dashboard />
-        <GoogleLoginComponent />
-      </FormContext.Provider>
+      <Router />
     </div>
   );
 };
