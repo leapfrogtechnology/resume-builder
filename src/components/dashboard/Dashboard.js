7020663bc9741774ebc16978b19e64ee4503b611
nikitashrestha <nikshres522@gmail.com>
2020-05-17 13:55:06 +0545

    login:#11 - add states and handlers

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import Header from '~/components/header/Header';
 import Skills from '~/components/skills/Skills';
 import Sidenav from '~/components/sidenav/Sidenav';
 import Certificate from '~/components/certificate/Certificate';
@@ -8,21 +9,54 @@ import WorkExperience from '~/components/workexperience/WorkExperience';
 import ProjectsUndertaken from '~/components/projectsundertaken/ProjectsUndertaken';
 import PersonalInformation from '~/components/personalinformation/PersonalInformation';
 
+import { FormContext } from '~/components/FormContext';
+import * as storageUtil from '~/storage/LocalStorage';
+
 const Dashboard = () => {
+  const [preview, setPreview] = useState(false);
+  const [username] = useState(storageUtil.getUser().name);
+  const [data, updateData] = useState({});
+
+  const togglePreview = () => setPreview(!preview);
+
+  const deleteCVHandler = () => {
+    storageUtil.deleteResume();
+    updateData({});
+  };
+
+  const store = {
+    preview: { get: preview, set: setPreview },
+    data: { get: data, set: updateData },
+    deleteCV: deleteCVHandler,
+  };
+
+  useEffect(() => {
+    const resume = storageUtil.getResume();
+
+    if (resume) {
+      updateData(prevState => ({ ...prevState, ...resume }));
+    } else {
+      updateData({});
+    }
+  }, []);
+
   return (
-    <section className="container">
-      <div className="main-container">
-        <div className="main-content">
-          <PersonalInformation />
-          <Skills />
-          <WorkExperience />
-          <ProjectsUndertaken />
-          <Achievements />
-          <Certificate />
+    <FormContext.Provider value={store}>
+      <Header name={username} status="Employee" onPreviewBtnClicked={togglePreview} />
+      <section className="container">
+        <div className="main-container">
+          <div className="main-content">
+            <PersonalInformation />
+            <Skills />
+            <WorkExperience />
+            <ProjectsUndertaken />
+            <Achievements />
+            <Certificate />
+          </div>
+          <Sidenav />
         </div>
-        <Sidenav />
-      </div>
-    </section>
+      </section>
+    </FormContext.Provider>
   );
 };
 
