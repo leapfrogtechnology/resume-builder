cef0229f09e8e9657d77e657ae32c20110db2905
nikitashrestha <nikshres522@gmail.com>
2020-05-17 13:49:52 +0545

    login:#11 - add error handler middleware

--- api/src/index.js
+++ api/src/index.js
@@ -2,26 +2,12 @@ import cors from 'cors';
 import morgan from 'morgan';
 import express from 'express';
 import bodyParser from 'body-parser';
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-import * as admin from 'firebase-admin';
 
 import routes from './routes';
 import logger from './utils/logger';
 
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_API_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
-
-// Initialize Firebase
-export const firebaseApp = admin.initializeApp(config);
+import json from './middlewares/json';
+import * as errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
@@ -37,12 +23,18 @@ app.locals.version = process.env.APP_VERSION;
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(errorHandler.bodyParser);
+app.use(json);
 
 /**
  * Routes Definitions
  */
 
 app.use('/api', routes);
+
+// Error Middlewares
+app.use(errorHandler.genericErrorHandler);
+app.use(errorHandler.methodNotAllowed);
 
 /**
  * Server Activation
