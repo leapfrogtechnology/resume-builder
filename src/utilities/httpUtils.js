1fc7d8be9c30bc2e201daa7618c311f3f5c271d0
nikitashrestha <nikshres522@gmail.com>
2020-05-17 14:07:36 +0545

    login:#11 - add utility to handle http request

---
+++ src/utilities/httpUtils.js
@@ -0,0 +1,103 @@
+import axios from 'axios';
+
+import textConstants from '~/constant/textConstants';
+import * as storageUtil from '~/storage/LocalStorage';
+
+/**
+ * Http Get.
+ *
+ * @param {*} url
+ * @param {*} [params={}]
+ * @returns {Promise}
+ */
+export function get(url, params = {}) {
+  const request = {
+    method: 'get',
+    url: url,
+    params: params,
+  };
+
+  return axios(request);
+}
+
+/**
+ * Http POST.
+ *
+ * @param {*} url
+ * @param {*} data
+ * @returns  {Promise}
+ */
+export function post(url, data) {
+  const request = {
+    method: 'post',
+    url: url,
+    data: data,
+  };
+
+  return axios(request);
+}
+
+/**
+ * Http Put.
+ *
+ * @param {*} url
+ * @param {*} data
+ * @returns {Promise}
+ */
+export function put(url, data) {
+  const request = {
+    method: 'put',
+    url: url,
+    data: data,
+  };
+
+  return axios(request);
+}
+
+/**
+ * Http Delete.
+ *
+ * @param {*} url
+ * @param {*} [data={}]
+ * @returns {Promise}
+ */
+export function remove(url, data = {}) {
+  const request = {
+    method: 'delete',
+    data,
+    url,
+  };
+
+  return axios(request);
+}
+
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (
+      error.response.status === textConstants.UNAUTHORIZED_CODE &&
+      error.response.data.error.message === textConstants.ACCESS_TOKEN_EXPIRE
+    ) {
+      storageUtil.logout();
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.request.use(
+  config => {
+    const accessToken = storageUtil.getAccessToken() || null;
+
+    if (accessToken !== null || accessToken !== undefined) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
