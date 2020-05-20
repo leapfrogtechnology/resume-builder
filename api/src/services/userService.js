f983c8c0520be30aead3270388b21134b22231fc
nikitashrestha <nikshres522@gmail.com>
2020-05-20 19:37:09 +0545

    login:#11 - add method to check if user is already logged in

--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -1,50 +1,96 @@
 import Boom from '@hapi/boom';
+
+import { db } from '../db';
+import * as tokenService from './tokenService';
+import * as sessionService from './sessionService';
+
 const { firebase, admin } = require('../utils/firebaseConfig');
 
 /**
- * Login user into firebase with google Id token and return firebase token if success.
+ * Check the firebase to see if user exist and if exists, return token. Otherwise, login the user into firebase and return token.
  *
  * @param {Object} data
  * @returns {Promise}
  */
-export async function loginUser(data) {
+export const loginUser = async (data) => {
   try {
-    const { idToken, email } = data;
+    const { idToken, email, name } = data;
 
-    const credential = firebase.auth.GoogleAuthProvider.credential(idToken);
+    const user = await fetchByEmail(email);
 
-    const userLogin = await firebase.auth().signInWithCredential(credential);
+    if (user && user.status) {
+      const credential = firebase.auth.GoogleAuthProvider.credential(idToken);
 
-    const token = userLogin.user.getIdToken();
+      const userLogin = await firebase.auth().signInWithCredential(credential);
+
+      const tokens = tokenService.generateTokens({ email: userLogin.email, uid: userLogin.user.uid });
+
+      const userInfo = {
+        user: {
+          uid: userLogin.user.uid,
+          name,
+          email,
+        },
+        tokens,
+      };
+
+      await createUser(userInfo.user);
+      await sessionService.createSession(userInfo);
+
+      return userInfo.tokens;
+    }
+
+    const tokens = tokenService.generateTokens({ email: user.email, uid: user.uid });
 
     const userInfo = {
       user: {
-        name: userLogin.user.displayName,
-        email: userLogin.user.email,
+        uid: user.uid,
+        name,
+        email,
       },
-      token: token,
+      tokens,
     };
 
-    return userInfo;
+    await sessionService.createSession(userInfo);
+
+    return userInfo.tokens;
   } catch (err) {
     console.error(err);
 
     throw err;
   }
-}
+};
 
+/**
+ * Fetch User by email.
+ *
+ * @param {*} email
+ * @returns {Promise}
+ */
 export async function fetchByEmail(email) {
   try {
     const result = await admin.auth().getUserByEmail(email);
 
-    if (!result) {
-      throw new Boom.notFound('User not registered');
-    }
-
     return result;
   } catch (err) {
+    if (err.code === 'auth/user-not-found') {
+      const result = {
+        status: 'NOT FOUND',
+      };
+      return result;
+    }
     console.error(err);
 
     throw err;
   }
 }
+
+export const createUser = (userInfo) => {
+  const userRef = db.ref(`users/${userInfo.uid}`);
+
+  return userRef.set({
+    name: userInfo.name,
+    email: userInfo.email,
+    resume: '',
+  });
+};
