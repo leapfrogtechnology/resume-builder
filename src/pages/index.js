2759d80316312c7b400e373ef60a22fd2ebc24bf
nikitashrestha <nikshres522@gmail.com>
2020-05-20 19:26:08 +0545

    login:#11 - import Dashboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,45 @@
+import Head from 'next/head';
 import React, { useState, useEffect } from 'react';
 
-import Head from 'next/head';
-import Router from '~/routes';
+import Header from '~/components/header/Header';
+import Skills from '~/components/skills/Skills';
+import Sidenav from '~/components/sidenav/Sidenav';
+import Certificate from '~/components/certificate/Certificate';
+import Achievements from '~/components/achievements/Achievements';
+import WorkExperience from '~/components/workexperience/WorkExperience';
+import ProjectsUndertaken from '~/components/projectsundertaken/ProjectsUndertaken';
+import PersonalInformation from '~/components/personalinformation/PersonalInformation';
 
-const App = () => {
-  const [loading, setLoading] = useState(true);
+import withAuth from '~/utilities/auth';
+import * as storageUtil from '~/storage/LocalStorage';
+import { FormContext } from '~/components/FormContext';
+
+const Dashboard = () => {
+  const [preview, setPreview] = useState(false);
+  const [data, updateData] = useState({});
+
+  const togglePreview = () => setPreview(!preview);
+
+  const deleteCVHandler = () => {
+    storageUtil.deleteResume();
+    updateData({});
+  };
+
+  const store = {
+    preview: { get: preview, set: setPreview },
+    data: { get: data, set: updateData },
+    deleteCV: deleteCVHandler,
+  };
 
   useEffect(() => {
-    setLoading(!loading);
-  }, []);
+    const resume = storageUtil.getResume();
 
-  if (loading) {
-    return null;
-  }
+    if (resume) {
+      updateData(prevState => ({ ...prevState, ...resume }));
+    } else {
+      updateData({});
+    }
+  }, []);
 
   return (
     <div className="page-container">
@@ -23,9 +50,24 @@ const App = () => {
         <title>ResumeBuilder</title>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.js"></script>
       </Head>
-      <Router />
+      <FormContext.Provider value={store}>
+        <Header name="Hello, Leapfrogger !" status="Employee" onPreviewBtnClicked={togglePreview} />
+        <section className="container">
+          <div className="main-container">
+            <div className="main-content">
+              <PersonalInformation />
+              <Skills />
+              <WorkExperience />
+              <ProjectsUndertaken />
+              <Achievements />
+              <Certificate />
+            </div>
+            <Sidenav />
+          </div>
+        </section>
+      </FormContext.Provider>
     </div>
   );
 };
 
-export default App;
+export default withAuth(Dashboard);
