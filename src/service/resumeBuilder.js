e0dd3027bd12e90f12330f6be75c538c00b0e152
nikitashrestha <nikshres522@gmail.com>
2020-05-22 09:40:55 +0545

    database-integrate:#7 - add service for save, fetch and delete resume

--- src/service/resumeBuilder.js
+++ src/service/resumeBuilder.js
@@ -10,3 +10,31 @@ export const validateUser = data => {
     resolve(result);
   });
 };
+
+export const saveResume = data => {
+  const { saveResumeUrl } = urlConstants;
+
+  return new Promise(resolve => {
+    const result = httpUtils.post(saveResumeUrl, data);
+
+    resolve(result);
+  });
+};
+
+export const fetchResume = async email => {
+  const fetchResumeUrl = urlConstants.apiBaseUrl + urlConstants.fetchResumeUrl + email;
+
+  const result = await httpUtils.get(fetchResumeUrl, {});
+
+  return result.data;
+};
+
+export const deleteResume = data => {
+  const deleteResumeUrl = urlConstants.apiBaseUrl + urlConstants.deleteResumeUrl;
+
+  return new Promise(resolve => {
+    const result = httpUtils.remove(deleteResumeUrl, data);
+
+    resolve(result);
+  });
+};
