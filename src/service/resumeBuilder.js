03e96b2a6f04e93fb6f67c789917d91ee4ac2748
nikitashrestha <nikshres522@gmail.com>
2020-05-24 13:01:39 +0545

    login:#11 - use http

--- src/service/resumeBuilder.js
+++ src/service/resumeBuilder.js
@@ -1,11 +1,11 @@
-import * as httpUtils from '~/utilities/httpUtils';
+import http from '~/utilities/httpUtils';
 import urlConstants from '~/constant/urlConstants';
 
 export const validateUser = data => {
   const { googleLoginUrl } = urlConstants;
 
   return new Promise(resolve => {
-    const result = httpUtils.post(googleLoginUrl, data);
+    const result = http.post(googleLoginUrl, data);
 
     resolve(result);
   });
