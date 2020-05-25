e949d66d14542fc0751241c362effb14f609ec23
nikitashrestha <nikshres522@gmail.com>
2020-05-25 13:56:26 +0545

    database-integrate:#7 - add get router

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -1,33 +1,13 @@
 import { Router } from 'express';
 
 import * as userController from '../controllers/users';
-import { findUser, userValidator } from '../validators/userValidator';
+import { ensureToken } from '../middlewares/ensureToken';
 
 const router = Router();
 
 /**
- * GET /api/users
+ * GET /api/users/:email
  */
-router.get('/', userController.fetchAll);
-
-/**
- * GET /api/users/:id
- */
-router.get('/:id', userController.fetchById);
-
-/**
- * POST /api/users
- */
-router.post('/', userValidator, userController.create);
-
-/**
- * PUT /api/users/:id
- */
-router.put('/:id', findUser, userValidator, userController.update);
-
-/**
- * DELETE /api/users/:id
- */
-router.delete('/:id', findUser, userController.deleteUser);
+router.get('/:email', ensureToken, userController.fetchUserProfile);
 
 export default router;
