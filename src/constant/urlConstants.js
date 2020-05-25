08dcb2c9625c395a2bfd263c6a803954e897cc71
nikitashrestha <nikshres522@gmail.com>
2020-05-25 13:51:12 +0545

    database-integrate:#7 - add url for resume-api-end-pints

--- src/constant/urlConstants.js
+++ src/constant/urlConstants.js
@@ -1,6 +1,8 @@
 const urlConstants = {
+  saveResumeUrl: '/resume',
   fetchResumeUrl: '/resume/',
   refreshTokenUrl: '/refresh',
+  fetchUserProfileUrl: '/users',
   googleLoginUrl: '/auth/google',
   deleteResumeUrl: '/resume/delete',
   apiBaseUrl: 'http://0.0.0.0:8848/api',
