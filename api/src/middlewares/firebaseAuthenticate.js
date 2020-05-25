a603b3608000722456a10648aeb147898dd0d19e
nikitashrestha <nikshres522@gmail.com>
2020-05-25 15:18:49 +0545

    database-integrate:#7 - add middleware to authenticate user in firebase

---
+++ api/src/middlewares/firebaseAuthenticate.js
@@ -0,0 +1,15 @@
+import { fetchByEmail } from '../services/userService';
+
+export const authenticateUser = async (req, res, next) => {
+  try {
+    const user = await fetchByEmail(req.params.email);
+
+    req.uid = user.uid;
+
+    next();
+  } catch (err) {
+    console.error(err);
+
+    next(err);
+  }
+};
