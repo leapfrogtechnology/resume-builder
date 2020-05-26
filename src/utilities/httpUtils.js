79c4f8ed7fc6b86e6bd4615b2932f5c10f6c78eb
nikitashrestha <nikshres522@gmail.com>
2020-05-26 18:38:49 +0545

    database-integrate:#7 - remove multiple and unncessary condition in if-else

--- src/utilities/httpUtils.js
+++ src/utilities/httpUtils.js
@@ -33,16 +33,9 @@ http.interceptors.response.use(
         return http(originalRequest);
       });
     } else if (
-      originalRequest.url !== urlConstants.googleLoginUrl &&
-      ((error.response.status === textConstants.UNAUTHORIZED_CODE &&
-        error.response.data.error &&
-        error.response.data.error.message === textConstants.REFRESH_TOKEN_EXPIRE) ||
-        (error.response.status === textConstants.NOT_FOUND &&
-          error.response.data.error &&
-          error.response.data.error.message === textConstants.USER_NOT_REGISTERED) ||
-        (error.response.status === textConstants.NOT_FOUND &&
-          error.response.data.error &&
-          error.response.data.error.message === textConstants.TOKEN_NOT_FOUND))
+      error.response.status === textConstants.UNAUTHORIZED_CODE &&
+      error.response.data.error &&
+      (error.response.data.error.message === textConstants.REFRESH_TOKEN_EXPIRE || textConstants.TOKEN_NOT_FOUND)
     ) {
       error.response.data.error.message = textConstants.SESSION_EXPIRED;
     }
@@ -55,7 +48,7 @@ http.interceptors.request.use(
   config => {
     const accessToken = localStorage.getAccessToken() || null;
 
-    if (accessToken !== null || accessToken !== undefined) {
+    if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
 
