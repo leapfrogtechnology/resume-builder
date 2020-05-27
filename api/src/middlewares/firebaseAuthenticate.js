602eacf718d2492e2fa0b302fc6970fd7cfb0951
nikitashrestha <nikshres522@gmail.com>
2020-05-27 09:54:03 +0545

    database-integrate:#7 - add comments

--- api/src/middlewares/firebaseAuthenticate.js
+++ api/src/middlewares/firebaseAuthenticate.js
@@ -1,10 +1,15 @@
 import { fetchByEmail } from '../services/userService';
 
+/**
+ * Authenticate User in firebase.
+ *
+ * @param {Object} req
+ * @param {Object} res
+ * @param {function} next
+ */
 export const authenticateUser = async (req, res, next) => {
   try {
-    const user = await fetchByEmail(req.params.email);
-
-    req.uid = user.uid;
+    const user = await fetchByEmail(req.email);
 
     next();
   } catch (err) {
