d8187aa582bed597e39b69cd5235ad5456f5bb00
nikitashrestha <nikshres522@gmail.com>
2020-05-27 14:04:45 +0545

    database-integrate:#7 - use async/await

--- api/src/middlewares/ensureToken.js
+++ api/src/middlewares/ensureToken.js
@@ -8,15 +8,17 @@ import * as tokenService from '../services/tokenService';
  * @param {Object} res
  * @param {function} next
  */
-export const ensureToken = (req, res, next) => {
+export const ensureToken = async (req, res, next) => {
   req.token = req.headers.authorization.substring(CONSTANT.BEARER_LENGTH);
 
-  tokenService
-    .verifyAccessToken(req.token)
-    .then((response) => {
-      req.email = response.encryptedData.email;
-      req.uid = response.encryptedData.uid;
-      next();
-    })
-    .catch((error) => next(error));
+  try {
+    const res = await tokenService.verifyAccessToken(req.token);
+
+    req.uid = res.encryptedData.uid;
+    req.email = res.encryptedData.email;
+
+    next();
+  } catch (err) {
+    next(err);
+  }
 };
