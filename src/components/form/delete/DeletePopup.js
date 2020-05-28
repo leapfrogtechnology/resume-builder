cdd05ed7a243262a6f6f1aaa2f2c8f08cb48a950
nikitashrestha <nikshres522@gmail.com>
2020-05-28 11:02:27 +0545

    temp-branch:#7 - add outside click detector

--- src/components/form/delete/DeletePopup.js
+++ src/components/form/delete/DeletePopup.js
@@ -3,28 +3,31 @@ import PropTypes from 'prop-types';
 
 import Button from '~/components/button/Button';
 import { WARNING } from '~/components/icons/icon';
+import OutsideClickDetector from '~/components/detector/OutsideClickDetector';
 
 const DeletePopup = ({ onConfirm, onCancel }) => {
   return (
     <div className="modal-container">
       <div className="delete-popup">
-        <div className="card">
-          <div className="delete-popup__content">
-            <div className="delete-popup__icon">{WARNING}</div>
-            <div className="delete-popup__body">
-              <div className="title">Are you sure?</div>
-              <p className="delete-popup__message">{`You are about to delete your resume. Do you want to proceed?`}</p>
-              <div className="form-button">
-                <div className="form-button__left">
-                  <Button content="Yes, Delete" onclick={onConfirm} modifier="danger" />
-                </div>
-                <div className="form-button__right">
-                  <Button content="Cancel" modifier="secondary" onclick={onCancel} />
+        <OutsideClickDetector onClose={onCancel}>
+          <div className="card">
+            <div className="delete-popup__content">
+              <div className="delete-popup__icon">{WARNING}</div>
+              <div className="delete-popup__body">
+                <div className="title">Are you sure?</div>
+                <p className="delete-popup__message">{`You are about to delete your resume. Do you want to proceed?`}</p>
+                <div className="form-button">
+                  <div className="form-button__left">
+                    <Button content="Yes, Delete" onclick={onConfirm} modifier="danger" />
+                  </div>
+                  <div className="form-button__right">
+                    <Button content="Cancel" modifier="secondary" onclick={onCancel} />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </OutsideClickDetector>
       </div>
     </div>
   );
