69db84682d1cb49a3f0ab6faca57e9e34933633b
nikitashrestha <nikshres522@gmail.com>
2020-05-28 12:01:20 +0545

    temp-branch:#7 - load .env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-const withImages = require('next-images');
 const path = require('path');
 const webpack = require('webpack');
+const withImages = require('next-images');
+const { parsed: localEnv } = require('dotenv').config();
 
 module.exports = withImages({
   webpack: config => {
     config.resolve.alias['~'] = path.resolve('src');
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+
     return config;
   },
 });
