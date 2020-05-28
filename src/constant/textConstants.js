088b05cc58eb7bd1b27efdd37d90e0e7360b6132
nikitashrestha <nikshres522@gmail.com>
2020-05-28 12:01:47 +0545

    temp-branch:#7 - use .env variable

--- src/constant/textConstants.js
+++ src/constant/textConstants.js
@@ -1,12 +1,9 @@
 const textConstants = {
-  //TODO
-  GOOGLE_CLIENT_ID: '378865161938-ks6ubfpsmrkumra9pqmv46775b1lof3j.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_AUTH_KEY_ERROR_MESSAGE: 'Please Check Your Google Client ID Auth Key',
-  DELETE_WARNING_DESCRIPTION: 'You will be deleting this bulletin segment!',
-  DELETE_USER_WARNING_DESCRIPTION: 'You will be deleting this User !',
   REFRESH_TOKEN_EXPIRE: 'Refresh Token Unauthorized',
   ACCESS_TOKEN_EXPIRE: 'Access Token Unauthorized',
-  DEFAULT_BULLETIN_TITLE: 'Leapfrog Resume Builder',
+  DEFAULT_TITLE: 'Leapfrog Resume Builder',
   SESSION_EXPIRED: 'Session Expired. Please Login.',
   USER_NOT_REGISTERED: 'User not registered',
   DELETE_WARNING_MESSAGE: 'Are you sure?',
