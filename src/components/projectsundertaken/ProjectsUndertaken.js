b2770ebb8de9dc8619bdb38a3755fd92f5e6fea5
nikitashrestha <nikshres522@gmail.com>
2020-06-03 18:16:51 +0545

    database-integrate:#7 - use id for edit, hide and delete of item

--- src/components/projectsundertaken/ProjectsUndertaken.js
+++ src/components/projectsundertaken/ProjectsUndertaken.js
@@ -1,111 +1,32 @@
-import React, { useContext, useState } from 'react';
+import React from 'react';
 
-import { Add } from '~/assets/image';
-import * as storage from '~/storage/LocalStorage';
-import { orderByDate } from '~/utilities/orderBy';
-import { FormContext } from '~/components/FormContext';
-import EmptyCard from '~/components/emptycard/EmptyCard';
-import CardHeader from '~/components/cardheader/CardHeader';
-import CardFooter from '~/components/cardfooter/CardFooter';
+import Section from '~/components/Section';
 import AddProject from '~/components/form/project/AddProject';
 import ProjectsUndertakenItem from '~/components/projectsundertaken/ProjectsUndertakenItem';
 
+import { orderByDate } from '~/utilities/orderBy';
+
 const ProjectsUndertaken = () => {
-  const context = useContext(FormContext);
-
-  const [addProject, setAdd] = useState(false);
-
-  const preview = context.preview.get;
-  const projects = context.data.get.projects;
-
-  const toggleAddProject = () => setAdd(!addProject);
-
-  /**
-   * Update the hidden state of project.
-   *
-   * @param {string} key [ name of a particular project].
-   */
-  const updateHiddenStateProject = key => {
-    const data = context.data.get;
-
-    data['projects'].find(({ name, hidden }, index) => {
-      if (name === key) {
-        const newState = !hidden;
-
-        data['projects'][index].hidden = newState;
-        context.data.set(data); // new state of data
-      }
-    });
-  };
-
-  const deleteProject = name => {
-    const data = context.data.get;
-
-    const filteredProjects = data['projects'].filter(project => {
-      return project.name !== name;
-    });
-
-    data['projects'] = filteredProjects;
-
-    context.data.set(prevState => ({ ...prevState, ...data }));
-
-    storage.saveResume(context.data.get);
-  };
-
-  if ((!projects || projects.length < 1) && preview) {
-    return <></>;
-  }
-
-  if (!projects || projects.length < 1) {
-    return (
-      <div className="content-block">
-        <div className="card">
-          <EmptyCard emptyMessage="You do not have any projects undertaken yet."></EmptyCard>
-          <CardFooter
-            icon={Add}
-            hide={preview}
-            label="Add another project"
-            showModal={addProject}
-            onAdd={toggleAddProject}
-            component={AddProject}
-            onClose={toggleAddProject}
-            modifier="empty"
-          />
-        </div>
-      </div>
-    );
-  }
-
-  const projectsList = orderByDate(projects).map(({ name, startDate, endDate, description, ongoing }, index) => (
-    <ProjectsUndertakenItem
-      key={index}
-      title={name}
-      startDate={startDate}
-      endDate={endDate}
-      ongoing={ongoing}
-      description={description}
-      preview={preview}
-      onHiddenIconClicked={updateHiddenStateProject}
-      onDelete={deleteProject}
-    />
-  ));
-
   return (
-    <div className="content-block">
-      <div className="card">
-        <CardHeader title="Projects Undertaken" />
-        <div className="projects-undertaken">{projectsList}</div>
-        <CardFooter
-          icon={Add}
-          hide={preview}
-          label="Add another project"
-          showModal={addProject}
-          onAdd={toggleAddProject}
-          component={AddProject}
-          onClose={toggleAddProject}
-        />
-      </div>
-    </div>
+    <Section dataKey="projects" component={AddProject}>
+      {({ data, preview, deleteItem, updateHiddenState }) =>
+        orderByDate(data).map(({ id, name, startDate, endDate, description, ongoing, hidden }) => (
+          <ProjectsUndertakenItem
+            key={id}
+            id={id}
+            title={name}
+            startDate={startDate}
+            endDate={endDate}
+            ongoing={ongoing}
+            description={description}
+            hidden={hidden}
+            preview={preview}
+            onHiddenIconClicked={updateHiddenState}
+            onDelete={deleteItem}
+          />
+        ))
+      }
+    </Section>
   );
 };
 
--- src/components/workexperience/WorkExperience.js
+++ src/components/workexperience/WorkExperience.js
@@ -1,59 +1,13 @@
-import React, { useContext, useState } from 'react';
+import React from 'react';
 
-import { Add } from '~/assets/image';
-import { FormContext } from '~/components/FormContext';
-import EmptyCard from '~/components/emptycard/EmptyCard';
-import CardHeader from '~/components/cardheader/CardHeader';
-import CardFooter from '~/components/cardfooter/CardFooter';
+import Section from '~/components/Section';
 import WorkExperienceShown from '~/components/workexperience/WorkExperienceShown';
 import AddWorkExperience from '~/components/form/workexperience/AddWorkExperience';
 
-import * as storage from '~/storage/LocalStorage';
 import { orderByDate } from '~/utilities/orderBy';
 import { baseMailToUrl, baseTelUrl } from '~/constant/contact';
 
 const WorkExperience = () => {
-  const context = useContext(FormContext);
-
-  const [addWork, setAdd] = useState(false);
-
-  const preview = context.preview.get;
-  const workExperience = context.data.get.workExperience;
-
-  const toggleAddWork = () => setAdd(!addWork);
-
-  /**
-   * Update the hidden state of work.
-   *
-   * @param {string} key [ name of a particular work experience].
-   */
-  const updateHiddenStateWork = key => {
-    const data = context.data.get;
-
-    data['workExperience'].find(({ name, hidden }, index) => {
-      if (name === key) {
-        const newState = !hidden;
-
-        data['workExperience'][index].hidden = newState;
-        context.data.set(data); // new state of data
-      }
-    });
-  };
-
-  const deleteWorkExperience = (name, position) => {
-    const data = context.data.get;
-
-    const filteredWorkExperiences = data['workExperience'].filter(work => {
-      return work.name !== name && work.position !== position;
-    });
-
-    data['workExperience'] = filteredWorkExperiences;
-
-    context.data.set(prevState => ({ ...prevState, ...data }));
-
-    storage.saveResume(context.data.get);
-  };
-
   const contactLinkHandler = value => {
     if (isNaN(value)) {
       window.open(baseMailToUrl + value);
@@ -62,72 +16,47 @@ const WorkExperience = () => {
     }
   };
 
-  if ((!workExperience || workExperience.length < 1) && preview) {
-    return <></>;
-  }
-
-  if (!workExperience || workExperience.length < 1) {
-    return (
-      <div className="content-block">
-        <div className="card">
-          <EmptyCard emptyMessage="You do not have any work experience yet."></EmptyCard>
-          <CardFooter
-            icon={Add}
-            hide={preview}
-            label="Add another work experience"
-            showModal={addWork}
-            onAdd={toggleAddWork}
-            component={AddWorkExperience}
-            onClose={toggleAddWork}
-            modifier="empty"
-          />
-        </div>
-      </div>
-    );
-  }
-
-  const workExperienceList = orderByDate(
-    workExperience
-  ).map(
-    (
-      { name, position, startDate, endDate, ongoing, responsibilities, achievements, refereeName, refereeContact },
-      index
-    ) => (
-      <WorkExperienceShown
-        key={index}
-        subTitle={name}
-        position={position}
-        startDate={startDate}
-        endDate={endDate}
-        roles={responsibilities}
-        achievements={achievements}
-        refereeName={refereeName}
-        refereeContact={refereeContact}
-        currentlyWorking={ongoing}
-        preview={preview}
-        onHiddenIconClicked={updateHiddenStateWork}
-        onDelete={deleteWorkExperience}
-        onContactLinkClicked={contactLinkHandler}
-      />
-    )
-  );
-
   return (
-    <div className="content-block">
-      <div className="card">
-        <CardHeader title="Work Experience" />
-        {workExperienceList}
-        <CardFooter
-          icon={Add}
-          hide={preview}
-          label="Add another work experience"
-          showModal={addWork}
-          onAdd={toggleAddWork}
-          component={AddWorkExperience}
-          onClose={toggleAddWork}
-        />
-      </div>
-    </div>
+    <Section dataKey="workExperience" component={AddWorkExperience}>
+      {({ data, preview, deleteItem, updateHiddenState }) =>
+        orderByDate(
+          data
+        ).map(
+          ({
+            id,
+            name,
+            position,
+            startDate,
+            endDate,
+            ongoing,
+            responsibilities,
+            achievements,
+            refereeName,
+            refereeContact,
+            hidden,
+          }) => (
+            <WorkExperienceShown
+              key={id}
+              id={id}
+              subTitle={name}
+              position={position}
+              startDate={startDate}
+              endDate={endDate}
+              roles={responsibilities}
+              achievements={achievements}
+              refereeName={refereeName}
+              refereeContact={refereeContact}
+              currentlyWorking={ongoing}
+              hidden={hidden}
+              preview={preview}
+              onHiddenIconClicked={updateHiddenState}
+              onDelete={deleteItem}
+              onContactLinkClicked={contactLinkHandler}
+            />
+          )
+        )
+      }
+    </Section>
   );
 };
 
