2c145b4d7f626981b1604ad8020907ed909b2368
nikitashrestha <nikshres522@gmail.com>
2020-06-03 16:37:50 +0545

    database-integrate:#7 - use uuid

--- src/utilities/objects/Achievement.js
+++ src/utilities/objects/Achievement.js
@@ -1,10 +1,13 @@
+import { v4 } from 'uuid';
+
 export const getAchievementObject = ({ name, date, description }) => {
   return {
+    id: v4(),
     name: name,
     date: date,
     description: description,
     hidden: false,
     isDeleted: false,
-    deletedOn: '1970-01-01T00:00:00.000Z',
+    deletedOn: null,
   };
 };
