55f2e7413c232ad170016cab6917d42743928471
nikitashrestha <nikshres522@gmail.com>
2020-06-05 10:03:05 +0545

    database-integrate:#7 - update props

--- src/components/userdetail/UserDetail.js
+++ src/components/userdetail/UserDetail.js
@@ -8,8 +8,10 @@ import OpenModal from '~/components/modal/OpenModal';
 import { FAVORITE_ICON } from '~/components/icons/icon';
 import AddExperience from '~/components/form/experience/AddExperience';
 
-const UserDetail = ({ name, experience, profileImg, preview, onPreviewBtnClicked }) => {
+const UserDetail = ({ name, experience, btnType, profileImg, preview, onBtnClicked }) => {
   const [showModal, setModal] = useState(false);
+
+  const btnLabel = btnType === 'preview' ? (!preview ? 'Preview' : 'Back To Edit') : 'Download as PDF';
 
   const toggleEdit = () => setModal(!showModal);
 
@@ -30,11 +32,9 @@ const UserDetail = ({ name, experience, profileImg, preview, onPreviewBtnClicked
             </div>
           </div>
         </div>
-        {onPreviewBtnClicked && (
-          <div className="user-detail__right-content">
-            <Button content={!preview ? 'Preview' : 'Back To Edit'} onclick={onPreviewBtnClicked} />
-          </div>
-        )}
+        <div className="user-detail__right-content">
+          <Button content={btnLabel} onclick={onBtnClicked} />
+        </div>
       </div>
     </section>
   );
@@ -43,9 +43,10 @@ const UserDetail = ({ name, experience, profileImg, preview, onPreviewBtnClicked
 UserDetail.propTypes = {
   name: PropTypes.string,
   experience: PropTypes.string,
+  btnType: PropTypes.string,
   profileImg: PropTypes.object,
   preview: PropTypes.bool,
-  onPreviewBtnClicked: PropTypes.func,
+  onBtnClicked: PropTypes.func,
 };
 
 export default UserDetail;
