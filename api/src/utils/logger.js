0d72cf28eb50a2c8e575054fe1a4aaa70e186995
Sparsha Dotel <sparshadotel@gmail.com>
2020-06-12 20:53:23 +0545

    Exclude creating logs directory

--- api/src/utils/logger.js
+++ api/src/utils/logger.js
@@ -7,10 +7,6 @@ import 'winston-daily-rotate-file';
 const LOG_DIR = process.env.LOG_DIR || 'logs';
 const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
 
-// Create log directory if it does not exist
-if (!fs.existsSync(LOG_DIR)) {
-  fs.mkdirSync(LOG_DIR);
-}
 
 /**
  * Create a new winston logger.
