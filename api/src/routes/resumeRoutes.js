3ad7d1b8a6b006c6813aeeffe95955e7ab400b8c
nikitashrestha <nikshres522@gmail.com>
2020-06-16 20:11:30 +0545

    dirty: Add api route to allow admin to edit resume

--- api/src/routes/resumeRoutes.js
+++ api/src/routes/resumeRoutes.js
@@ -2,6 +2,7 @@ import Router from 'express';
 
 import { ensureToken } from '../middlewares/ensureToken';
 import * as resumeController from '../controllers/resume';
+import { authenticateAdmin } from '../middlewares/authenticateAdmin';
 import { authenticateUser } from '../middlewares/firebaseAuthenticate';
 
 const router = Router();
@@ -10,6 +11,11 @@ const router = Router();
  * PUT /api/resume
  */
 router.put('/', ensureToken, authenticateUser, resumeController.updateResume);
+
+/**
+ * PUT /resume/edit/:email
+ */
+router.put('/edit/:email', ensureToken, authenticateAdmin, resumeController.updateResumeByAdmin);
 
 /**
  * GET /api/resume/:email
