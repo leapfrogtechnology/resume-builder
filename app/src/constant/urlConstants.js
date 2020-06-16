2628747cac0cbbccbe8b686abef5c6a790da036f
nikitashrestha <nikshres522@gmail.com>
2020-06-16 20:13:36 +0545

    dirty: Add editResumeByAdminUrl

--- app/src/constant/urlConstants.js
+++ app/src/constant/urlConstants.js
@@ -5,6 +5,7 @@ const urlConstants = {
   fetchResumeUrl: '/resume/:email',
   deleteResumeUrl: '/resume/delete',
   fetchUserProfileUrl: '/users/self',
+  editResumeByAdminUrl: '/resume/edit/:email',
   apiBaseUrl: process.env.API_BASE_URL,
 };
 
