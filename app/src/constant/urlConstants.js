5d6ce697c8b3f80f3ce82ac8c63d4a2735ea9dfe
nikitashrestha <nikshres522@gmail.com>
2020-06-17 19:53:32 +0545

    admin-feature: Update urls

--- app/src/constant/urlConstants.js
+++ app/src/constant/urlConstants.js
@@ -1,10 +1,11 @@
 const urlConstants = {
-  saveResumeUrl: '/resume',
   refreshTokenUrl: '/refresh',
   googleLoginUrl: '/auth/google',
-  fetchResumeUrl: '/resume/:email',
   deleteResumeUrl: '/resume/delete',
+  saveResumeUrl: '/resume/self/save',
   fetchUserProfileUrl: '/users/self',
+  fetchResumeUrl: '/resume/:email/preview',
+  editResumeByAdminUrl: '/resume/:email/edit',
   apiBaseUrl: process.env.API_BASE_URL,
 };
 
