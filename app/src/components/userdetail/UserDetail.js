a93defaa508100da48f0e59e8e20554c196b9c4c
nikitashrestha <nikshres522@gmail.com>
2020-06-17 19:56:18 +0545

    admin-feature: Update props and propsTypes

--- app/src/components/userdetail/UserDetail.js
+++ app/src/components/userdetail/UserDetail.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 
 import { Image } from '~/assets/image';
 import Media from '~/components/media/Media';
@@ -8,10 +9,17 @@ import OpenModal from '~/components/modal/OpenModal';
 import { FAVORITE_ICON } from '~/components/icons/icon';
 import AddExperience from '~/components/form/experience/AddExperience';
 
-const UserDetail = ({ name, experience, btnType, profileImg, preview, onBtnClicked }) => {
+const UserDetail = ({
+  name,
+  experience,
+  profileImg,
+  preview,
+  showPreviewBtn,
+  downloadBtnHandler,
+  previewBtnHandler,
+}) => {
+  const router = useRouter();
   const [showModal, setModal] = useState(false);
-
-  const btnLabel = btnType === 'preview' ? (!preview ? 'Preview' : 'Back To Edit') : 'Download as PDF';
 
   const toggleEdit = () => setModal(!showModal);
 
@@ -33,7 +41,8 @@ const UserDetail = ({ name, experience, btnType, profileImg, preview, onBtnClick
           </div>
         </div>
         <div className="user-detail__right-content">
-          <Button content={btnLabel} onclick={onBtnClicked} />
+          {showPreviewBtn && <Button content={!preview ? 'Preview' : 'Back To Edit'} onclick={previewBtnHandler} />}
+          {preview && router.query.email && <Button content="Download as PDF" onclick={downloadBtnHandler} />}
         </div>
       </div>
     </section>
@@ -43,10 +52,11 @@ const UserDetail = ({ name, experience, btnType, profileImg, preview, onBtnClick
 UserDetail.propTypes = {
   name: PropTypes.string,
   experience: PropTypes.string,
-  btnType: PropTypes.string,
   profileImg: PropTypes.object,
   preview: PropTypes.bool,
-  onBtnClicked: PropTypes.func,
+  showPreviewBtn: PropTypes.bool,
+  downloadBtnHandler: PropTypes.func || null,
+  previewBtnHandler: PropTypes.func || null,
 };
 
 export default UserDetail;
